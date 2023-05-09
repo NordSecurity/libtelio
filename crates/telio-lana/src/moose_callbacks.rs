2cc79d0810a68fff1a9ce6fc452933a24b41560e
Hasan Mahmood <hasan.mahmood@nordsec.com>
2023-05-09 11:40:08 +0000

    Move moosecallback in a separate module

---
+++ crates/telio-lana/src/moose_callbacks.rs
@@ -0,0 +1,32 @@
+pub use moosemeshnetapp as moose;
+pub use telio_utils::{telio_log_error, telio_log_info, telio_log_warn};
+
+/// Struct for Moose callbacks
+pub struct MooseCallbacks;
+
+impl moose::InitCallback for MooseCallbacks {
+    fn on_init(&self, result_code: &Result<moose::ContextState, moose::MooseError>) {
+        match result_code {
+            Ok(res) => telio_log_info!("Moose init success with code {:?}", res),
+            Err(err) => telio_log_error!("Moose init failed with code {:?}", err),
+        }
+    }
+}
+
+impl moose::ErrorCallback for MooseCallbacks {
+    fn on_error(
+        &self,
+        error_level: moose::MooseErrorLevel,
+        error_code: moose::MooseError,
+        msg: &str,
+    ) {
+        match error_level {
+            moose::MooseErrorLevel::Warning => {
+                telio_log_warn!("Moose error {:?}: {}", error_code, msg)
+            }
+            moose::MooseErrorLevel::Error => {
+                telio_log_error!("Moose error {:?}: {}", error_code, msg)
+            }
+        }
+    }
+}
