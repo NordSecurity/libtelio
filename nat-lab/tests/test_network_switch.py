99576ff4adeb5165e97ae446d85dd44a597153a3
Michal Ziobro <michal.ziobro@nordsec.com>
2023-07-03 11:47:30 +0000

    [LLT-318] Add network change test for direct

--- nat-lab/tests/test_network_switch.py
+++ nat-lab/tests/test_network_switch.py
@@ -1,7 +1,9 @@
 from utils import Ping, stun
 from contextlib import AsyncExitStack
 from mesh_api import API
+from config import DERP_PRIMARY, DERP_SECONDARY, DERP_TERTIARY, DERP_SERVERS
 from telio import AdapterType, PathType
+from telio_features import TelioFeatures, Direct
 import asyncio
 import config
 import pytest
@@ -22,7 +24,7 @@ from utils import (
         pytest.param(
             ConnectionTag.DOCKER_SHARED_CLIENT_1,
             "10.0.254.1",
-            "10.0.254.2",
+            "10.0.254.13",
         ),
         pytest.param(
             ConnectionTag.WINDOWS_VM,
@@ -220,3 +222,131 @@ async def test_vpn_network_switch(
 
         ip = await testing.wait_long(stun.get(connection, config.STUN_SERVER))
         assert ip == wg_server["ipv4"], f"wrong public IP when connected to VPN {ip}"
+
+
+@pytest.mark.asyncio
+@pytest.mark.timeout(150)
+@pytest.mark.parametrize(
+    "endpoint_providers, alpha_connection_tag, adapter_type, notify_network_change",
+    [
+        pytest.param(
+            ["stun"],
+            ConnectionTag.DOCKER_SHARED_CLIENT_1,
+            AdapterType.BoringTun,
+            True,
+        ),
+        pytest.param(
+            ["stun"],
+            ConnectionTag.DOCKER_SHARED_CLIENT_1,
+            AdapterType.LinuxNativeWg,
+            True,
+            marks=pytest.mark.linux_native,
+        ),
+        pytest.param(
+            ["stun"],
+            ConnectionTag.WINDOWS_VM,
+            AdapterType.WindowsNativeWg,
+            True,
+            marks=pytest.mark.windows,
+        ),
+        pytest.param(
+            ["stun"],
+            ConnectionTag.WINDOWS_VM,
+            AdapterType.WireguardGo,
+            True,
+            marks=pytest.mark.windows,
+        ),
+    ],
+)
+async def test_mesh_network_switch_direct(
+    endpoint_providers, alpha_connection_tag, adapter_type, notify_network_change
+) -> None:
+    async with AsyncExitStack() as exit_stack:
+        api = API()
+        (alpha, beta) = api.default_config_two_nodes()
+
+        (alpha_connection, network_switcher) = await exit_stack.enter_async_context(
+            new_connection_with_network_switcher(alpha_connection_tag)
+        )
+        assert network_switcher
+
+        beta_connection = await exit_stack.enter_async_context(
+            new_connection_by_tag(ConnectionTag.DOCKER_CONE_CLIENT_2)
+        )
+
+        alpha_client = await exit_stack.enter_async_context(
+            telio.run_meshnet(
+                alpha_connection,
+                alpha,
+                api.get_meshmap(alpha.id),
+                adapter_type,
+                telio_features=TelioFeatures(
+                    direct=Direct(providers=endpoint_providers)
+                ),
+            )
+        )
+
+        beta_client = await exit_stack.enter_async_context(
+            telio.run_meshnet(
+                beta_connection,
+                beta,
+                api.get_meshmap(beta.id),
+                telio.AdapterType.BoringTun,
+                telio_features=TelioFeatures(
+                    direct=Direct(providers=endpoint_providers)
+                ),
+            )
+        )
+
+        await testing.wait_lengthy(
+            asyncio.gather(
+                alpha_client.wait_for_any_derp_state(
+                    [telio.State.Connected],
+                ),
+                beta_client.wait_for_any_derp_state(
+                    [telio.State.Connected],
+                ),
+            ),
+        )
+
+        await testing.wait_lengthy(
+            asyncio.gather(
+                alpha_client.handshake(
+                    beta.public_key,
+                    telio.PathType.Direct,
+                ),
+                beta_client.handshake(
+                    alpha.public_key,
+                    telio.PathType.Direct,
+                ),
+            ),
+        )
+
+        async with Ping(alpha_connection, beta.ip_addresses[0]) as ping:
+            await testing.wait_long(ping.wait_for_next_ping())
+
+        await network_switcher.switch_to_secondary_network()
+
+        await alpha_client.notify_network_change()
+
+        await testing.wait_lengthy(
+            alpha_client.wait_for_any_derp_state(
+                [telio.State.Connected],
+            ),
+        )
+
+        await testing.wait_lengthy(
+            asyncio.gather(
+                alpha_client.handshake(
+                    beta.public_key,
+                    telio.PathType.Direct,
+                ),
+                beta_client.handshake(
+                    alpha.public_key,
+                    telio.PathType.Direct,
+                ),
+            ),
+        )
+
+        async with Ping(alpha_connection, beta.ip_addresses[0]) as ping:
+            await testing.wait_lengthy(ping.wait_for_next_ping())
