fa6d8895e131e52591d8d95231b45ab4ca053bf8
Gytis Stoškevičius <gytisstoskevicius@gmail.com>
2023-08-18 16:52:33 +0300

    Remove natlab test order and improve stability

--- nat-lab/conftest.py
+++ nat-lab/conftest.py
@@ -1,25 +1,15 @@
-import pytest
+import os
 
 
-def pytest_collection_modifyitems(items):
-    # Separate test execution into layers based on platform. The biggest reason
-    # for this is to separate `linux_native` tests. If the tests are running
-    # normally, they are interleaved, e.g.:
-    # Test1-BoringTun, Test1-LinuxNative, Test2-BoringTun, Test2-LinuxNative.
-    # For whatever reason, `linux_native` tests that connect to VPN server take
-    # 30 seconds to run, if they are executed after the same BoringTun test.
-    # Given 4 tests like that, interleaving increases test execution time by 2 minutes.
-
-    for item in items:
-        order_index = 1
-        for mark in item.iter_markers():
-            if mark.name == "linux_native":
-                order_index = 0
-            elif mark.name == "windows":
-                order_index = 2
-            elif mark.name == "mac":
-                order_index = 3
-            elif mark.name == "long":
-                order_index = 4
-
-        item.add_marker(pytest.mark.order(order_index))
+def pytest_runtest_setup():
+    os.system("docker exec nat-lab-vpn-01-1 bash -c 'wg setconf wg0 /wg0.conf'")
+    os.system("docker exec nat-lab-vpn-02-1 bash -c 'wg setconf wg0 /wg0.conf'")
+    os.system(
+        "docker exec -d nat-lab-derp-01-1 bash -c 'pkill nordderper ; nordderper'"
+    )
+    os.system(
+        "docker exec -d nat-lab-derp-02-1 bash -c 'pkill nordderper ; nordderper'"
+    )
+    os.system(
+        "docker exec -d nat-lab-derp-03-1 bash -c 'pkill nordderper ; nordderper'"
+    )
