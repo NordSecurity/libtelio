984bf780e72c8765d7c5b29886240917c74fa2c6
Gytis Stoškevičius <gytisstoskevicius@gmail.com>
2023-08-25 14:18:33 +0300

    Restore derp server preparation before test

--- nat-lab/conftest.py
+++ nat-lab/conftest.py
@@ -1,6 +1,7 @@
-import natlab
+import os
 
 
 def pytest_runtest_setup(item):
     if any(mark for mark in item.iter_markers() if mark.name == "derp"):
-        natlab.quick_restart_container(["derp"])
+        for derp in ["nat-lab-derp-01-1", "nat-lab-derp-02-1", "nat-lab-derp-03-1"]:
+            os.system(f"docker exec -d {derp} bash -c 'pkill nordderper ; nordderper'")
