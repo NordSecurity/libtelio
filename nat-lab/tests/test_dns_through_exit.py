46107211e6df5fbc172405dc374cb3b9b0aa82f6
Gytis Stoškevičius <gytisstoskevicius@gmail.com>
2023-09-01 13:24:54 +0300

    update dns through exit test with setup func

--- nat-lab/tests/test_dns_through_exit.py
+++ nat-lab/tests/test_dns_through_exit.py
@@ -4,16 +4,12 @@ import pytest
 import re
 import telio
 from contextlib import AsyncExitStack
-from mesh_api import API
-from telio import AdapterType, State
+from helpers import setup_mesh_nodes, SetupParameters
+from telio import State
 from typing import List, Tuple
 from utils import testing
 from utils.connection_tracker import ConnectionLimits
-from utils.connection_util import (
-    generate_connection_tracker_config,
-    ConnectionTag,
-    new_connection_with_conn_tracker,
-)
+from utils.connection_util import generate_connection_tracker_config, ConnectionTag
 from utils.router import IPStack
 
 
@@ -56,25 +52,60 @@ from utils.router import IPStack
     ],
 )
 @pytest.mark.parametrize(
-    "alpha_connection_tag,alpha_adapter_type",
+    "alpha_setup_params",
     [
-        pytest.param(ConnectionTag.DOCKER_CONE_CLIENT_1, AdapterType.BoringTun),
         pytest.param(
-            ConnectionTag.DOCKER_CONE_CLIENT_1,
-            AdapterType.LinuxNativeWg,
+            SetupParameters(
+                connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_1,
+                adapter_type=telio.AdapterType.BoringTun,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.DOCKER_CONE_CLIENT_1,
+                    derp_1_limits=ConnectionLimits(1, 1),
+                ),
+            )
+        ),
+        pytest.param(
+            SetupParameters(
+                connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_1,
+                adapter_type=telio.AdapterType.LinuxNativeWg,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.DOCKER_CONE_CLIENT_1,
+                    derp_1_limits=ConnectionLimits(1, 1),
+                ),
+            ),
             marks=pytest.mark.linux_native,
         ),
         # This test is failing, but currently is non critical
         # pytest.param(
-        #     ConnectionTag.MAC_VM,
-        #     AdapterType.BoringTun,
+        #     SetupParameters(
+        #         connection_tag=ConnectionTag.MAC_VM,
+        #         adapter_type=telio.AdapterType.BoringTun,
+        #         connection_tracker_config=generate_connection_tracker_config(
+        #             ConnectionTag.MAC_VM,
+        #             derp_1_limits=ConnectionLimits(1, 1),
+        #         ),
+        #     ),
         #     marks=pytest.mark.mac,
         # ),
     ],
 )
+@pytest.mark.parametrize(
+    "beta_setup_params",
+    [
+        pytest.param(
+            SetupParameters(
+                connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_2,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.DOCKER_CONE_CLIENT_2,
+                    derp_1_limits=ConnectionLimits(1, 1),
+                ),
+            )
+        )
+    ],
+)
 async def test_dns_through_exit(
-    alpha_connection_tag: ConnectionTag,
-    alpha_adapter_type: AdapterType,
+    alpha_setup_params: SetupParameters,
+    beta_setup_params: SetupParameters,
     alpha_info: Tuple[IPStack, List[str]],
     exit_info: Tuple[IPStack, List[str]],
 ) -> None:
@@ -96,56 +127,13 @@ async def test_dns_through_exit(
             else config.LIBTELIO_DNS_IPV6
         )
 
-        api = API()
-
-        (alpha, exit_node) = api.default_config_two_nodes(
-            alpha_ip_stack=alpha_info[0], beta_ip_stack=exit_info[0]
+        env = await setup_mesh_nodes(
+            exit_stack, [alpha_setup_params, beta_setup_params]
         )
 
-        (connection_alpha, alpha_conn_tracker) = await exit_stack.enter_async_context(
-            new_connection_with_conn_tracker(
-                alpha_connection_tag,
-                generate_connection_tracker_config(
-                    alpha_connection_tag, derp_1_limits=ConnectionLimits(1, 1)
-                ),
-            )
-        )
-        (connection_exit, exit_conn_tracker) = await exit_stack.enter_async_context(
-            new_connection_with_conn_tracker(
-                ConnectionTag.DOCKER_CONE_CLIENT_2,
-                generate_connection_tracker_config(
-                    ConnectionTag.DOCKER_CONE_CLIENT_2,
-                    derp_1_limits=ConnectionLimits(1, 1),
-                ),
-            )
-        )
-
-        client_alpha = await exit_stack.enter_async_context(
-            telio.Client(connection_alpha, alpha, alpha_adapter_type).run(
-                api.get_meshmap(alpha.id)
-            )
-        )
-
-        client_exit = await exit_stack.enter_async_context(
-            telio.Client(connection_exit, exit_node).run(api.get_meshmap(exit_node.id))
-        )
-
-        await testing.wait_lengthy(
-            asyncio.gather(
-                client_alpha.wait_for_state_on_any_derp([State.Connected]),
-                client_exit.wait_for_state_on_any_derp([State.Connected]),
-                alpha_conn_tracker.wait_for_event("derp_1"),
-                exit_conn_tracker.wait_for_event("derp_1"),
-            )
-        )
-        await testing.wait_lengthy(
-            asyncio.gather(
-                client_alpha.wait_for_state_peer(
-                    exit_node.public_key, [State.Connected]
-                ),
-                client_exit.wait_for_state_peer(alpha.public_key, [State.Connected]),
-            )
-        )
+        _, exit_node = env.nodes
+        client_alpha, client_exit = env.clients
+        connection_alpha, _ = [conn.connection for conn in env.connections]
 
         # entry connects to exit
         await testing.wait_long(client_exit.get_router().create_exit_node_route())
@@ -218,6 +206,3 @@ async def test_dns_through_exit(
             )
             is not None
         )
-
-        assert alpha_conn_tracker.get_out_of_limits() is None
-        assert exit_conn_tracker.get_out_of_limits() is None
