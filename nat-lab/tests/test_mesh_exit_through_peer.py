91de64be445e8c119b7fc78bb9d617d48553552e
Gytis Stoškevičius <gytisstoskevicius@gmail.com>
2023-09-04 11:51:48 +0300

    update exit through peer with setup funcs

--- nat-lab/tests/test_mesh_exit_through_peer.py
+++ nat-lab/tests/test_mesh_exit_through_peer.py
@@ -3,8 +3,9 @@ import config
 import pytest
 import telio
 from contextlib import AsyncExitStack
+from helpers import setup_mesh_nodes, SetupParameters
 from mesh_api import API
-from telio import AdapterType, State
+from telio import State, AdapterType
 from utils import testing, stun
 from utils.connection_tracker import ConnectionLimits
 from utils.connection_util import (
@@ -34,86 +35,100 @@ PHOTO_ALBUM_IPV6 = config.LIBTELIO_IPV6_WAN_SUBNET + "::adda:edde:5"
     ],
 )
 @pytest.mark.parametrize(
-    "alpha_connection_tag,adapter_type",
+    "alpha_setup_params",
     [
-        pytest.param(ConnectionTag.DOCKER_CONE_CLIENT_1, telio.AdapterType.BoringTun),
         pytest.param(
-            ConnectionTag.DOCKER_CONE_CLIENT_1,
-            telio.AdapterType.LinuxNativeWg,
+            SetupParameters(
+                connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_1,
+                ip_stack=IPStack.IPv4v6,
+                adapter_type=telio.AdapterType.BoringTun,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.DOCKER_CONE_CLIENT_1,
+                    derp_1_limits=ConnectionLimits(1, 1),
+                ),
+            )
+        ),
+        pytest.param(
+            SetupParameters(
+                connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_1,
+                ip_stack=IPStack.IPv4v6,
+                adapter_type=telio.AdapterType.LinuxNativeWg,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.DOCKER_CONE_CLIENT_1,
+                    derp_1_limits=ConnectionLimits(1, 1),
+                ),
+            ),
             marks=pytest.mark.linux_native,
         ),
         pytest.param(
-            ConnectionTag.WINDOWS_VM,
-            telio.AdapterType.WindowsNativeWg,
-            marks=[
-                pytest.mark.windows,
-                pytest.mark.xfail(reason="Test is flaky - LLT-4357"),
-            ],
-        ),
-        pytest.param(
-            ConnectionTag.WINDOWS_VM,
-            telio.AdapterType.WireguardGo,
+            SetupParameters(
+                connection_tag=ConnectionTag.WINDOWS_VM,
+                ip_stack=IPStack.IPv4v6,
+                adapter_type=telio.AdapterType.WindowsNativeWg,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.WINDOWS_VM,
+                    derp_1_limits=ConnectionLimits(1, 1),
+                ),
+            ),
             marks=pytest.mark.windows,
         ),
         pytest.param(
-            ConnectionTag.MAC_VM, telio.AdapterType.Default, marks=pytest.mark.mac
+            SetupParameters(
+                connection_tag=ConnectionTag.WINDOWS_VM,
+                ip_stack=IPStack.IPv4v6,
+                adapter_type=telio.AdapterType.WireguardGo,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.WINDOWS_VM,
+                    derp_1_limits=ConnectionLimits(1, 1),
+                ),
+            ),
+            marks=pytest.mark.windows,
+        ),
+        pytest.param(
+            SetupParameters(
+                connection_tag=ConnectionTag.MAC_VM,
+                ip_stack=IPStack.IPv4v6,
+                adapter_type=telio.AdapterType.BoringTun,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.MAC_VM,
+                    derp_1_limits=ConnectionLimits(1, 1),
+                ),
+            ),
+            marks=pytest.mark.mac,
         ),
     ],
 )
-async def test_mesh_exit_through_peer(
-    alpha_connection_tag: ConnectionTag,
-    adapter_type: AdapterType,
-    exit_ip_stack: IPStack,
-) -> None:
-    async with AsyncExitStack() as exit_stack:
-        api = API()
-
-        (alpha, beta) = api.default_config_two_nodes(
-            alpha_ip_stack=IPStack.IPv4v6, beta_ip_stack=exit_ip_stack
-        )
-        (connection_alpha, alpha_conn_tracker) = await exit_stack.enter_async_context(
-            new_connection_with_conn_tracker(
-                alpha_connection_tag,
-                generate_connection_tracker_config(
-                    alpha_connection_tag, derp_1_limits=ConnectionLimits(1, 1)
-                ),
-            )
-        )
-        (connection_beta, beta_conn_tracker) = await exit_stack.enter_async_context(
-            new_connection_with_conn_tracker(
-                ConnectionTag.DOCKER_CONE_CLIENT_2,
-                generate_connection_tracker_config(
+@pytest.mark.parametrize(
+    "beta_setup_params",
+    [
+        pytest.param(
+            SetupParameters(
+                connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_2,
+                connection_tracker_config=generate_connection_tracker_config(
                     ConnectionTag.DOCKER_CONE_CLIENT_2,
                     derp_1_limits=ConnectionLimits(1, 1),
                     stun_limits=ConnectionLimits(1, 2),
                 ),
             )
         )
-
-        client_alpha = await exit_stack.enter_async_context(
-            telio.Client(connection_alpha, alpha, adapter_type).run(
-                api.get_meshmap(alpha.id)
-            )
+    ],
+)
+async def test_mesh_exit_through_peer(
+    alpha_setup_params: SetupParameters,
+    beta_setup_params: SetupParameters,
+    exit_ip_stack: IPStack,
+) -> None:
+    async with AsyncExitStack() as exit_stack:
+        beta_setup_params.ip_stack = exit_ip_stack
+        env = await setup_mesh_nodes(
+            exit_stack, [alpha_setup_params, beta_setup_params]
         )
 
-        client_beta = await exit_stack.enter_async_context(
-            telio.Client(connection_beta, beta).run(api.get_meshmap(beta.id))
-        )
-
-        await testing.wait_lengthy(
-            asyncio.gather(
-                client_alpha.wait_for_state_on_any_derp([State.Connected]),
-                client_beta.wait_for_state_on_any_derp([State.Connected]),
-                alpha_conn_tracker.wait_for_event("derp_1"),
-                beta_conn_tracker.wait_for_event("derp_1"),
-            )
-        )
-        await testing.wait_lengthy(
-            asyncio.gather(
-                client_alpha.wait_for_state_peer(beta.public_key, [State.Connected]),
-                client_beta.wait_for_state_peer(alpha.public_key, [State.Connected]),
-            )
-        )
+        _, beta = env.nodes
+        client_alpha, client_beta = env.clients
+        connection_alpha, connection_beta = [
+            conn.connection for conn in env.connections
+        ]
 
         if exit_ip_stack in [IPStack.IPv4, IPStack.IPv4v6]:
             async with Ping(
@@ -138,10 +153,7 @@ async def test_mesh_exit_through_peer(
         ip_alpha = await testing.wait_long(
             stun.get(connection_alpha, config.STUN_SERVER)
         )
-        await testing.wait_long(beta_conn_tracker.wait_for_event("stun"))
-
         ip_beta = await testing.wait_long(stun.get(connection_beta, config.STUN_SERVER))
-        await testing.wait_long(beta_conn_tracker.wait_for_event("stun"))
 
         assert ip_alpha == ip_beta
 
@@ -179,9 +191,6 @@ async def test_mesh_exit_through_peer(
                     beta.public_key, list(State), list(telio.PathType)
                 )
             )
-
-        assert alpha_conn_tracker.get_out_of_limits() is None
-        assert beta_conn_tracker.get_out_of_limits() is None
 
 
 @pytest.mark.parametrize(
@@ -253,13 +262,13 @@ async def test_ipv6_exit_node(
         )
 
         client_alpha = await exit_stack.enter_async_context(
-            telio.Client(connection_alpha, alpha, adapter_type).run_meshnet(
+            telio.Client(connection_alpha, alpha, adapter_type).run(
                 api.get_meshmap(alpha.id)
             )
         )
 
         client_beta = await exit_stack.enter_async_context(
-            telio.Client(connection_beta, beta).run_meshnet(api.get_meshmap(beta.id))
+            telio.Client(connection_beta, beta).run(api.get_meshmap(beta.id))
         )
 
         await testing.wait_lengthy(
