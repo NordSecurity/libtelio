11a2c8e0eb7de9ce5f119c25c663111c8d23696e
Matas Misiunas <matislovas@gmail.com>
2023-11-29 12:19:41 +0200

    Fix qos to use secondary address to ping

--- crates/telio-nurse/src/rtt/ping.rs
+++ crates/telio-nurse/src/rtt/ping.rs
@@ -1,7 +1,11 @@
+use socket2::Type;
+use std::net::{Ipv4Addr, Ipv6Addr};
 use std::sync::Arc;
 use std::time::Duration;
 use std::{convert::TryInto, net::IpAddr};
-use surge_ping::{Client, Config, PingIdentifier, PingSequence, ICMP};
+use surge_ping::{
+    Client, Config as PingerConfig, ConfigBuilder, PingIdentifier, PingSequence, ICMP,
+};
 use telio_utils::{telio_log_debug, telio_log_error, DualTarget};
 
 use crate::qos::NodeInfo;
@@ -38,8 +42,8 @@ impl Ping {
     /// * `no_of_tries` - How many pings should be sent.
     pub fn new(no_of_tries: u32) -> std::io::Result<Self> {
         Ok(Self {
-            client_v4: Arc::new(Client::new(&Config::default())?),
-            client_v6: Arc::new(Client::new(&Config::builder().kind(ICMP::V6).build())?),
+            client_v4: Arc::new(Client::new(&Self::make_builder(ICMP::V4).build())?),
+            client_v6: Arc::new(Client::new(&Self::make_builder(ICMP::V6).build())?),
             no_of_tries,
         })
     }
@@ -89,54 +93,28 @@ impl Ping {
     async fn perform_average_rtt(&self, target: &DualTarget) -> DualPingResults {
         let mut dpresults = DualPingResults { v4: None, v6: None };
 
-        let ping_action = |client: Arc<Client>, host| async move {
-            let mut results = PingResults {
-                successful_pings: 0,
-                unsuccessful_pings: 0,
-                avg_rtt: None,
-            };
-            let mut pinger = client.pinger(host, PingIdentifier(rand::random())).await;
-            let payload = [0; 56];
-            pinger.timeout(Self::PING_TIMEOUT);
-
-            let mut sum = Duration::default();
-
-            for i in 0..self.no_of_tries {
-                if let Ok((_, duration)) = pinger
-                    .ping(PingSequence(i.try_into().unwrap_or(0)), &payload)
-                    .await
-                {
-                    sum = sum.saturating_add(duration);
-                    results.successful_pings += 1;
-                } else {
-                    results.unsuccessful_pings += 1;
-                }
-            }
-
-            results.avg_rtt = sum.checked_div(results.successful_pings);
-            results
-        };
-
         match target.get_targets() {
             Ok(t) => {
                 match t.0 {
                     IpAddr::V4(_) => {
-                        dpresults.v4 = Some(ping_action(self.client_v4.clone(), t.0).await);
+                        dpresults.v4 = Some(self.ping_action(t.0).await);
                     }
                     IpAddr::V6(_) => {
-                        dpresults.v6 = Some(ping_action(self.client_v6.clone(), t.0).await);
+                        dpresults.v6 = Some(self.ping_action(t.0).await);
                     }
                 }
 
                 if let Some(secondary) = t.1 {
                     match secondary {
                         IpAddr::V4(_) => {
-                            dpresults.v4 = Some(ping_action(self.client_v4.clone(), t.0).await);
+                            dpresults.v4 = Some(self.ping_action(secondary).await);
                         }
                         IpAddr::V6(_) => {
-                            dpresults.v6 = Some(ping_action(self.client_v6.clone(), t.0).await);
+                            dpresults.v6 = Some(self.ping_action(secondary).await);
                         }
                     }
+                } else {
+                    telio_log_debug!("No secondary target");
                 }
             }
             Err(_) => {
@@ -145,5 +123,75 @@ impl Ping {
         }
 
         dpresults
+    }
+
+    async fn ping_action(&self, host: IpAddr) -> PingResults {
+        let mut results = PingResults {
+            successful_pings: 0,
+            unsuccessful_pings: 0,
+            avg_rtt: None,
+        };
+
+        telio_log_debug!("Trying to ping {:?} host", host);
+
+        let mut pinger = match host {
+            IpAddr::V4(_) => {
+                self.client_v4
+                    .clone()
+                    .pinger(host, PingIdentifier(rand::random()))
+                    .await
+            }
+            IpAddr::V6(_) => {
+                self.client_v6
+                    .clone()
+                    .pinger(host, PingIdentifier(rand::random()))
+                    .await
+            }
+        };
+
+        pinger.timeout(Self::PING_TIMEOUT);
+
+        let mut sum = Duration::default();
+
+        for i in 0..self.no_of_tries {
+            match pinger
+                .ping(PingSequence(i.try_into().unwrap_or(0)), &[0; 56])
+                .await
+            {
+                Ok((_, duration)) => {
+                    sum = sum.saturating_add(duration);
+                    results.successful_pings += 1;
+                }
+                Err(e) => {
+                    results.unsuccessful_pings += 1;
+                    telio_log_error!("Ping error: {}", e.to_string());
+                }
+            }
+        }
+
+        results.avg_rtt = sum.checked_div(results.successful_pings);
+        results
+    }
+
+    fn make_builder(proto: ICMP) -> ConfigBuilder {
+        let mut config_builder = PingerConfig::builder().kind(proto);
+        if cfg!(any(
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "tvos",
+        )) {
+            config_builder = config_builder.sock_type_hint(Type::RAW);
+        }
+        if cfg!(not(target_os = "android")) {
+            match proto {
+                ICMP::V4 => {
+                    config_builder = config_builder.bind((Ipv4Addr::UNSPECIFIED, 0).into());
+                }
+                ICMP::V6 => {
+                    config_builder = config_builder.bind((Ipv6Addr::UNSPECIFIED, 0).into());
+                }
+            }
+        }
+        config_builder
     }
 }
