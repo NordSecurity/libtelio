ef148126ea405c6b3904de8bf7c4361fe7d4424e
Gytis Stoškevičius <gytisstoskevicius@gmail.com>
2024-03-06 16:33:04 +0200

    Add libtelio versioning script and version placeholder
    Since there will be build staging, some sort of build
    versioning tool is needed, so we can replace version
    string in binary without rebuilding it

--- crates/telio-utils/src/git.rs
+++ crates/telio-utils/src/git.rs
@@ -4,11 +4,17 @@ pub fn commit_sha() -> &'static str {
     option_env!("LIBTELIO_COMMIT_SHA").unwrap_or("dev")
 }
 
-/// Get the version tag that was used when building.
-/// If not present defaults to "dev".
+/// Get the version placeholder (half of the maximum length of git tag)
+/// Will be replaced during build promotions
+#[inline(never)]
+#[allow(index_access_check)]
 pub fn version_tag() -> &'static str {
-    option_env!("LIBTELIO_COMMIT_TAG")
-        .map(|tag| tag.trim())
-        .and_then(|tag| if tag.is_empty() { None } else { Some(tag) })
-        .unwrap_or("dev")
+    const VER: [u8;129] = *b"VERSION_PLACEHOLDER@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\0";
+    match VER.iter().position(|v| *v == 0) {
+        Some(i) => match std::str::from_utf8(&VER[..i]) {
+            Ok(s) => s,
+            Err(_) => "not_a_utf8_string",
+        },
+        None => "incorrect_version_string",
+    }
 }
