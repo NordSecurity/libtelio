ba60c0d82145e87903fb8ceed482fccca38a8666
Jakub Janowski <jakub.janowski@nordsec.com>
2024-04-05 12:04:22 +0300

    Revert "Synchronize racey configure_route.sh"
    This reverts commit b98310a702db4c4c9cfb62fac6f44fd72bc5d04b.
    The bug with missing interfaces was not related to any race condition.
    It was a bug in docker compose:
    https://github.com/docker/compose/issues/11510
    The only affected version is: 2.24.6

--- nat-lab/bin/configure_route.sh
+++ nat-lab/bin/configure_route.sh
@@ -6,42 +6,9 @@ print_help() {
     exit 1
 }
 
-# There are a bunch of nightly failures on new Vagrant runners #13, #14, #15, and #16, e.g.
-#
-# [2024-03-19 12:47:09] |EXECUTE| docker compose logs internal-symmetric-gw-01
-# [2024-03-19 12:47:10] internal-symmetric-gw-01-1  | + case "${1:-}" in
-# [2024-03-19 12:47:10] internal-symmetric-gw-01-1  | + ip route delete 10.0.0.0/16
-# [2024-03-19 12:47:10] internal-symmetric-gw-01-1  | RTNETLINK answers: No such process
-# [2024-03-19 12:47:10] internal-symmetric-gw-01-1  | + true
-# [2024-03-19 12:47:10] internal-symmetric-gw-01-1  | + ip route add 10.0.0.0/16 via 192.168.103.254 dev eth1
-# [2024-03-19 12:47:10] internal-symmetric-gw-01-1  | Cannot find device "eth1"
-#
-# Its probably because the new runners have much more powerfull CPUs, and this causes some type
-# of race condition somewhere in Docker/Compose. Seems like entrypoint is somehow executed first
-# before interfaces have been fully configured. To try and workaround this, allow a little bit of
-# time for interfaces to be fully initialized.
-wait_for_interface() {
-    interface=$1
-
-    for i in {0..10}; do
-        if ip a show "$interface" up | grep -q inet; then
-            echo "Network interface $interface is up."
-            return
-        else
-            echo "Waiting for network interface $interface to be up..."
-            sleep 0.2
-        fi
-    done
-
-    echo "Timeout reached. Network interface $interface is not up."
-    exit 1
-}
-
 case "${1:-}" in
     primary)
         if [[ $CLIENT_GATEWAY_PRIMARY != "none" ]]; then
-            wait_for_interface eth0
-
             ip route delete 10.0.0.0/16 || true
             ip route add 10.0.0.0/16 via $CLIENT_GATEWAY_PRIMARY dev eth0
 
@@ -52,8 +19,6 @@ case "${1:-}" in
         fi
         ;;
     secondary)
-        wait_for_interface eth1
-
         ip route delete 10.0.0.0/16 || true
         ip route add 10.0.0.0/16 via $CLIENT_GATEWAY_SECONDARY dev eth1
         ;;
