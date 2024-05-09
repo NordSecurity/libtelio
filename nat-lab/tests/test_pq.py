384d41536fcc60ab96000488966e8352ef3493f2
Mateusz Szczygieł <mateusz.szczygiel@nordsec.com>
2024-05-09 09:37:34 +0300

    PQ: Add nat-lab rekeying test
    Signed-off-by: Mateusz Szczygieł <mateusz.szczygiel@nordsec.com>

--- nat-lab/tests/test_pq.py
+++ nat-lab/tests/test_pq.py
@@ -3,10 +3,14 @@ import pytest
 from contextlib import AsyncExitStack
 from helpers import SetupParameters, setup_environment
 from telio import AdapterType, Client
-from utils import stun
+from utils import stun, testing
 from utils.connection import Connection
 from utils.connection_tracker import ConnectionLimits
-from utils.connection_util import generate_connection_tracker_config, ConnectionTag
+from utils.connection_util import (
+    generate_connection_tracker_config,
+    ConnectionTag,
+    new_connection_by_tag,
+)
 from utils.ping import Ping
 
 
@@ -28,6 +32,17 @@ async def _connect_vpn_pq(
 
     ip = await stun.get(client_conn, config.STUN_SERVER)
     assert ip == wg_server["ipv4"], f"wrong public IP when connected to VPN {ip}"
+
+
+async def inspect_preshared_key(nlx_conn: Connection) -> str:
+    output = await testing.wait_normal(
+        nlx_conn.create_process(["nlx", "show", "nordlynx0", "dump"]).execute()
+    )
+    last = output.get_stdout().splitlines()[-1]
+    preshared = last.split()[1]
+
+    assert preshared != "(none)", "Preshared key is not assigned"
+    return preshared
 
 
 # TODO(LLT-4979): Enable boringtun adapters. Currently, disabled enabled because of a bug inside of boringtun.
@@ -130,3 +145,125 @@ async def test_pq_vpn_connection(
             client_conn,
             client_alpha,
         )
+
+
+@pytest.mark.parametrize(
+    "alpha_setup_params, public_ip",
+    [
+        # pytest.param(
+        #     SetupParameters(
+        #         connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_1,
+        #         adapter_type=AdapterType.BoringTun,
+        #         connection_tracker_config=generate_connection_tracker_config(
+        #             ConnectionTag.DOCKER_CONE_CLIENT_1,
+        #             stun_limits=ConnectionLimits(1, 1),
+        #             nlx_1_limits=ConnectionLimits(2, 2),
+        #         ),
+        #         is_meshnet=False,
+        #     ),
+        #     "10.0.254.1",
+        # ),
+        pytest.param(
+            SetupParameters(
+                connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_1,
+                adapter_type=AdapterType.LinuxNativeWg,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.DOCKER_CONE_CLIENT_1,
+                    stun_limits=ConnectionLimits(1, 1),
+                    nlx_1_limits=ConnectionLimits(2, 2),
+                ),
+                is_meshnet=False,
+            ),
+            "10.0.254.1",
+            marks=pytest.mark.linux_native,
+        ),
+        pytest.param(
+            SetupParameters(
+                connection_tag=ConnectionTag.WINDOWS_VM_1,
+                adapter_type=AdapterType.WindowsNativeWg,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.WINDOWS_VM_1,
+                    stun_limits=ConnectionLimits(1, 1),
+                    nlx_1_limits=ConnectionLimits(2, 2),
+                ),
+                is_meshnet=False,
+            ),
+            "10.0.254.7",
+            marks=[
+                pytest.mark.windows,
+            ],
+        ),
+        pytest.param(
+            SetupParameters(
+                connection_tag=ConnectionTag.WINDOWS_VM_1,
+                adapter_type=AdapterType.WireguardGo,
+                connection_tracker_config=generate_connection_tracker_config(
+                    ConnectionTag.WINDOWS_VM_1,
+                    stun_limits=ConnectionLimits(1, 1),
+                    nlx_1_limits=ConnectionLimits(2, 2),
+                ),
+                is_meshnet=False,
+            ),
+            "10.0.254.7",
+            marks=[
+                pytest.mark.windows,
+            ],
+        ),
+        # pytest.param(
+        #     SetupParameters(
+        #         connection_tag=ConnectionTag.MAC_VM,
+        #         adapter_type=AdapterType.BoringTun,
+        #         connection_tracker_config=generate_connection_tracker_config(
+        #             ConnectionTag.MAC_VM,
+        #             stun_limits=ConnectionLimits(1, 1),
+        #             nlx_1_limits=ConnectionLimits(2, 2),
+        #         ),
+        #         is_meshnet=False,
+        #     ),
+        #     "10.0.254.7",
+        #     marks=pytest.mark.mac,
+        # ),
+    ],
+)
+async def test_pq_vpn_rekey(
+    alpha_setup_params: SetupParameters,
+    public_ip: str,
+) -> None:
+    # Set rekey interval to some small value
+    alpha_setup_params.features.post_quantum_vpn.rekey_interval_s = 2
+
+    async with AsyncExitStack() as exit_stack:
+        env = await exit_stack.enter_async_context(
+            setup_environment(exit_stack, [alpha_setup_params])
+        )
+
+        client_conn, *_ = [conn.connection for conn in env.connections]
+        client_alpha, *_ = env.clients
+
+        ip = await stun.get(client_conn, config.STUN_SERVER)
+        assert ip == public_ip, f"wrong public IP before connecting to VPN {ip}"
+
+        await _connect_vpn_pq(
+            client_conn,
+            client_alpha,
+        )
+
+        nlx_conn = await exit_stack.enter_async_context(
+            new_connection_by_tag(ConnectionTag.DOCKER_NLX_1)
+        )
+
+        preshared_before = inspect_preshared_key(nlx_conn)
+        await testing.wait_long(client_alpha.wait_for_log("Successful PQ REKEY"))
+
+        preshared_after = inspect_preshared_key(nlx_conn)
+        assert (
+            preshared_after != preshared_before
+        ), "Preshared key not changed on the nlx server"
+
+        ip = await stun.get(client_conn, config.STUN_SERVER)
+        assert (
+            ip == config.NLX_SERVER["ipv4"]
+        ), f"wrong public IP when connected to VPN {ip}"
+
+        async with Ping(client_conn, config.PHOTO_ALBUM_IP).run() as ping:
+            await ping.wait_for_next_ping()
