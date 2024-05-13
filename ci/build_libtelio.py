db879851f30443b63dde7d5584d920e9614d45c8
Matas Misiunas <matislovas@gmail.com>
2024-05-13 15:30:23 +0300

    Add build_libtelio.py fetch binaries feature

--- ci/build_libtelio.py
+++ ci/build_libtelio.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+import getpass
+import json
 import os
+import re
 import sys
 import subprocess
 import moose_utils
+import requests
 import shutil
+import zipfile
+from datetime import datetime
 from pathlib import Path
 
 NAME = "telio"
@@ -249,12 +255,139 @@ LIBTELIO_CONFIG = {
 }
 
 
+def fetch_build_artifacts(tag_prefix, target_os, target_arch, token, silent=False):
+    def extract_date(tag, tag_prefix):
+        """Extract the date from the tag name."""
+        date_char_count = 6
+
+        if tag_prefix == "main":
+            date_char_count = 10
+
+        date_str = re.search(f"{tag_prefix}-([0-9]{{{date_char_count}}})", tag).group(1)
+        return date_str
+
+    def get_latest_tag(tag_prefix):
+        """Find the latest tag with the given prefix."""
+        subprocess.run(
+            ["git", "-C", PROJECT_ROOT, "fetch", "--tags", "--quiet"], check=True
+        )
+
+        tags = (
+            subprocess.check_output(
+                ["git", "-C", PROJECT_ROOT, "tag", "--sort=-creatordate"]
+            )
+            .decode()
+            .splitlines()
+        )
+        tags = [tag for tag in tags if tag.startswith(f"{tag_prefix}-")]
+
+        latest_tag = None
+        latest_date = None
+
+        for tag in tags:
+            date_str = extract_date(tag, tag_prefix)
+            date = datetime.strptime(
+                date_str, "%y%m%d" if len(date_str) == 6 else "%y%m%d%H%M"
+            )
+            if latest_date is None or date > latest_date:
+                latest_tag = tag
+                latest_date = date
+
+        if latest_tag:
+            message = (
+                subprocess.check_output(
+                    ["git", "-C", PROJECT_ROOT, "tag", "-l", "-n1", latest_tag]
+                )
+                .decode()
+                .strip()
+                .split(" ", 1)[1]
+            )
+            return latest_tag, message
+        else:
+            return None, None
+
+    def get_remote_path() -> str:
+        LIBTELIO_BUILD_PROJECT_ID = 6299
+        libtelio_env_sec_gitlab_repository = os.environ.get(
+            "LIBTELIO_ENV_SEC_GITLAB_REPOSITORY", None
+        )
+
+        if libtelio_env_sec_gitlab_repository is None:
+            raise ValueError("LIBTELIO_ENV_SEC_GITLAB_REPOSITORY not set.")
+
+        return f"https://{libtelio_env_sec_gitlab_repository}/api/v4/projects/{LIBTELIO_BUILD_PROJECT_ID}"
+
+    def get_api(path, timeout=300):
+        with requests.get(
+            get_remote_path() + path,
+            headers={"PRIVATE-TOKEN": token if token else ""},
+            timeout=timeout,
+        ) as request:
+            request.raise_for_status()
+            response_string = request.content.decode("utf-8")
+            return response_string
+
+    def get_artifacts(path_to_save, job, timeout=300, unzip=False):
+        full_path = path_to_save + job["artifacts_file"]["filename"]
+
+        if not silent:
+            print("Getting artficats for ", job["name"], ", filename: ", full_path)
+
+        r = requests.get(
+            get_remote_path() + "/jobs/" + str(job["id"]) + "/artifacts",
+            headers={"PRIVATE-TOKEN": token if token else ""},
+            timeout=timeout,
+        )
+        with open(str(full_path), "wb") as f:
+            f.write(r.content)
+
+        with zipfile.ZipFile(full_path, "r") as zip_ref:
+            zip_ref.extractall(path_to_save)
+
+    def get_pipeline_build_artifacts(pipeline_id, path_to_save, target_arch, target_os):
+        for job in json.loads(
+            get_api(
+                (
+                    f"/pipelines/{pipeline_id}/jobs?per_page=100&include_retried=true&scope=success"
+                )
+            )
+        ):
+            if job["stage"] == "build":
+                if target_os == "uniffi" and job["name"] == "uniffi-bindings":
+                    get_artifacts(path_to_save, job)
+                    return True
+                else:
+                    if (
+                        target_os in job["name"] if target_os is not None else True
+                    ) and target_arch in job["name"]:
+                        get_artifacts(path_to_save, job, unzip=True)
+                        return True
+
+        return False
+
+    tag, tag_msg = get_latest_tag(tag_prefix)
+    if tag_msg:
+        tag_json = json.loads(tag_msg)
+        get_pipeline_build_artifacts(
+            tag_json["pipeline_id"], PROJECT_ROOT, target_arch, target_os
+        )
+        return True
+    else:
+        print(f"No {tag_prefix} tag found.")
+        return False
+
+
 def main() -> None:
     parser = rutils.create_cli_parser()
     build_parser = parser._subparsers._group_actions[0].choices["build"]
     build_parser.add_argument("--moose", action="store_true", help="Use libmoose")
     build_parser.add_argument(
         "--msvc", action="store_true", help="Use MSVC toolchain for Windows build"
+    )
+    build_parser.add_argument(
+        "--try-fetch-from-pipeline",
+        choices=["main", "nightly", "staging"],
+        help="pipeline tag in gitlab.",
     )
     build_parser.add_argument(
         "--uniffi-test-bindings",
@@ -318,6 +451,20 @@ def main() -> None:
 
 
 def exec_build(args):
+    # Try fetching from pipeline, if it fails, continue with the build
+    if args.try_fetch_from_pipeline:
+        if "LLH_GROUP_TOKEN_FLAKY_TESTS" in os.environ:
+            token = os.environ["LLH_GROUP_TOKEN_FLAKY_TESTS"]
+        else:
+            token = getpass.getpass("Enter Gitlab API access token:")
+        if fetch_build_artifacts(
+            args.try_fetch_from_pipeline, args.os, args.arch, token
+        ):
+            if args.moose and args.os in ["linux", "windows", "android"]:
+                moose_utils.fetch_moose_dependencies(args.os, MOOSE_MAP[args.arch])
+
+            return 0
+
     if args.moose:
         if args.os in ["linux", "windows", "android"]:
             sys.path.append(f"{PROJECT_ROOT}/ci")
