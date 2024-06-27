97931b2b446a8c1f15545398c7d5f09c00dc397f
Hasan <hasan.mahmood@nordsec.com>
2024-06-27 15:36:51 +0200

    Fix windows module

--- crates/telio-network-monitors/src/windows.rs
+++ crates/telio-network-monitors/src/windows.rs
@@ -1,33 +1,34 @@
-use std::ffi::c_void;
-use std::ffi::c_void;
+use crate::monitor::PATH_CHANGE_BROADCAST;
 use std::os::raw::c_ulong;
 use std::ptr;
-use std::ptr;
-use telio_utils::{telio_log_debug, telio_log_error};
-use winapi::shared::minwindef::{BOOL, DWORD, TRUE};
+use telio_utils::{telio_log_debug, telio_log_warn};
+use winapi::shared::netioapi::{
+    CancelMibChangeNotify2, NotifyIpInterfaceChange, MIB_IPINTERFACE_ROW,
+};
+use winapi::shared::ntdef::{HANDLE, PVOID};
 use winapi::shared::ws2def::AF_UNSPEC;
-use winapi::um::iptypes::MIB_IPINTERFACE_ROW;
-use winapi::um::netioapi::{CancelMibChangeNotify2, NotifyIpInterfaceChange};
-use winapi::um::winnt::{HANDLE, PVOID};
-use winapi::um::{iphlpapi::NotifyAddrChange, minwinbase::OVERLAPPED, Foundation::HANDLE};
 
 unsafe extern "system" fn callback(
     caller_context: PVOID,
     row: *mut MIB_IPINTERFACE_ROW,
     notification_type: c_ulong,
 ) {
-    println!("Network interface change detected.");
+    if let Err(e) = PATH_CHANGE_BROADCAST.send(()) {
+        telio_log_warn!("Failed to notify about changed path {e}");
+    }
+    telio_log_debug!("Network interface change detected.");
 }
 
-fn handle_windows() {
+/// Method to setup network monitoring for Windows
+pub fn setup_network_monitor() {
     unsafe {
         let mut handle: HANDLE = ptr::null_mut();
 
         let result = NotifyIpInterfaceChange(
             AF_UNSPEC as u16, // Family: AF_UNSPEC for all, or AF_INET, AF_INET6 for specific
             Some(callback),   // Callback function
-            ptr::null(),      // Caller context
-            false,            // Initial notification
+            ptr::null_mut(),  // Caller context
+            0,                // Initial notification
             &mut handle,      // Notification handle
         );
 
@@ -41,14 +42,14 @@ fn handle_windows() {
         }
 
         // Clean up the notification
-        let cancel_result = CancelMibChangeNotify2(handle);
-        if cancel_result == 0 {
-            println!("Notification canceled successfully.");
-        } else {
-            println!(
-                "Failed to cancel notification with error code: {}",
-                cancel_result
-            );
-        }
+        // let cancel_result = CancelMibChangeNotify2(handle);
+        // if cancel_result == 0 {
+        //     println!("Notification canceled successfully.");
+        // } else {
+        //     println!(
+        //         "Failed to cancel notification with error code: {}",
+        //         cancel_result
+        //     );
+        // }
     }
 }
