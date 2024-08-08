53f8e0db3562470230e1d69126cbd1a892721de7
Tomasz Grzegowski <tomasz.grzegowski@nordsec.com>
2024-08-08 15:39:07 +0200

    Add debug logs for mutlicast

--- nat-lab/tests/test_multicast_connection.py
+++ nat-lab/tests/test_multicast_connection.py
@@ -6,6 +6,7 @@ from telio_features import TelioFeatures
 from typing import List, Tuple
 from utils.connection_util import ConnectionTag, Connection, TargetOS
 from utils.multicast import MulticastClient, MulticastServer
+from config import LIBTELIO_BINARY_PATH_MAC_VM
 
 
 def generate_setup_parameter_pair(
@@ -24,34 +25,34 @@ def generate_setup_parameter_pair(
 
 
 MUILTICAST_TEST_PARAMS = [
-    pytest.param(
-        generate_setup_parameter_pair([
-            (ConnectionTag.DOCKER_FULLCONE_CLIENT_1, AdapterType.BoringTun),
-            (ConnectionTag.DOCKER_FULLCONE_CLIENT_2, AdapterType.BoringTun),
-        ]),
-        "ssdp",
-    ),
-    pytest.param(
-        generate_setup_parameter_pair([
-            (ConnectionTag.DOCKER_SYMMETRIC_CLIENT_1, AdapterType.BoringTun),
-            (ConnectionTag.DOCKER_SYMMETRIC_CLIENT_2, AdapterType.BoringTun),
-        ]),
-        "mdns",
-    ),
-    pytest.param(
-        generate_setup_parameter_pair([
-            (ConnectionTag.WINDOWS_VM_1, AdapterType.WireguardGo),
-            (ConnectionTag.DOCKER_CONE_CLIENT_1, AdapterType.BoringTun),
-        ]),
-        "ssdp",
-    ),
-    pytest.param(
-        generate_setup_parameter_pair([
-            (ConnectionTag.DOCKER_CONE_CLIENT_1, AdapterType.BoringTun),
-            (ConnectionTag.WINDOWS_VM_1, AdapterType.WindowsNativeWg),
-        ]),
-        "mdns",
-    ),
+    # pytest.param(
+    #     generate_setup_parameter_pair([
+    #         (ConnectionTag.DOCKER_FULLCONE_CLIENT_1, AdapterType.BoringTun),
+    #         (ConnectionTag.DOCKER_FULLCONE_CLIENT_2, AdapterType.BoringTun),
+    #     ]),
+    #     "ssdp",
+    # ),
+    # pytest.param(
+    #     generate_setup_parameter_pair([
+    #         (ConnectionTag.DOCKER_SYMMETRIC_CLIENT_1, AdapterType.BoringTun),
+    #         (ConnectionTag.DOCKER_SYMMETRIC_CLIENT_2, AdapterType.BoringTun),
+    #     ]),
+    #     "mdns",
+    # ),
+    # pytest.param(
+    #     generate_setup_parameter_pair([
+    #         (ConnectionTag.WINDOWS_VM_1, AdapterType.WireguardGo),
+    #         (ConnectionTag.DOCKER_CONE_CLIENT_1, AdapterType.BoringTun),
+    #     ]),
+    #     "ssdp",
+    # ),
+    # pytest.param(
+    #     generate_setup_parameter_pair([
+    #         (ConnectionTag.DOCKER_CONE_CLIENT_1, AdapterType.BoringTun),
+    #         (ConnectionTag.WINDOWS_VM_1, AdapterType.WindowsNativeWg),
+    #     ]),
+    #     "mdns",
+    # ),
     pytest.param(
         generate_setup_parameter_pair([
             (ConnectionTag.MAC_VM, AdapterType.BoringTun),
@@ -70,6 +71,7 @@ MUILTICAST_TEST_PARAMS = [
 
 
 async def add_multicast_route(connection: Connection) -> None:
+    print("Adding multicast routes")
     if connection.target_os == TargetOS.Linux:
         ipconf = connection.create_process(
             ["ip", "route", "add", "224.0.0.0/4", "dev", "tun10"]
@@ -95,6 +97,22 @@ async def test_multicast(setup_params: List[SetupParameters], protocol: str) -> 
         await add_multicast_route(alpha_connection)
         await add_multicast_route(beta_connection)
 
+        process = await alpha_connection.create_process([
+            "which",
+            "python3",
+        ]).execute()
+        stdout = process.get_stdout()
+        print(f"python3: {stdout}")
+
+        process = await alpha_connection.create_process([
+            "find",
+            ".",
+            # LIBTELIO_BINARY_PATH_MAC_VM,
+        ]).execute()
+        stdout = process.get_stdout()
+        print(f"{LIBTELIO_BINARY_PATH_MAC_VM}: {stdout}")
+
         async with MulticastServer(beta_connection, protocol).run() as server:
             await server.wait_till_ready()
+            print("Multicast server is ready")
             await MulticastClient(alpha_connection, protocol).execute()
