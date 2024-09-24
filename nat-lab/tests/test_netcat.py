0e990a393b6f550d6383b53cf3bc5bbf75180180
Tomasz Grzegowski <tomasz.grzegowski@nordsec.com>
2024-09-24 10:39:57 +0000

    Smoke test

--- nat-lab/tests/test_netcat.py
+++ nat-lab/tests/test_netcat.py
@@ -2,39 +2,28 @@ import asyncio
 import pytest
 from contextlib import AsyncExitStack
 from utils.connection_util import ConnectionTag, new_connection_by_tag
-from utils.netcat import NetCat, NetCatServer, NetCatClient
+from utils.netcat import NetCatServer, NetCatClient
 
 TEST_STRING = "test data"
 PORT = 12345
 
+
 @pytest.mark.asyncio
-@pytest.mark.timeout(30)
+@pytest.mark.timeout(10)
 async def test_netcat() -> None:
     async with AsyncExitStack() as exit_stack:
-        connection_1 = await exit_stack.enter_async_context(
-            new_connection_by_tag(ConnectionTag.DOCKER_OPEN_INTERNET_CLIENT_1)
+        connection = await exit_stack.enter_async_context(
+            new_connection_by_tag(ConnectionTag.DOCKER_CONE_CLIENT_1)
         )
+        server_ip = (await connection.get_ip_address())[1]
 
-        connection_2 = await exit_stack.enter_async_context(
-            new_connection_by_tag(ConnectionTag.DOCKER_OPEN_INTERNET_CLIENT_2)
-        )
-
-        server_ip = await connection_1.get_ip_address()
-        print(server_ip)
-
-        async with NetCatServer(connection_1, PORT).run() as server:
+        async with NetCatServer(connection, PORT).run() as server:
             await server.listening_started()
-            async with NetCatClient(connection_2, PORT, server_ip[1]).run() as client:
-                # await client.connection_succeeded()
-                # await server.connection_received()
-
+            async with NetCatClient(connection, server_ip, PORT).run() as client:
                 await asyncio.gather(
-                    server.connection_received(),
-                    client.connection_succeeded()
+                    server.connection_received(), client.connection_succeeded()
                 )
 
                 await client.send_data(TEST_STRING)
-
                 server_data = await server.receive_data()
-                print("DATA", server_data)
                 assert server_data == TEST_STRING
--- nat-lab/tests/utils/netcat.py
+++ nat-lab/tests/utils/netcat.py
@@ -1,25 +1,47 @@
 import asyncio
 import platform
-from utils.connection import Connection
-from utils.process import Process
-from utils.output_notifier import OutputNotifier
-from datetime import datetime
-from typing import Optional, List, Dict, AsyncIterator
 from contextlib import asynccontextmanager
+from datetime import datetime
+from typing import Optional, AsyncIterator
+from utils.connection import Connection
+from utils.output_notifier import OutputNotifier
+from utils.process import Process
 
 
 class NetCat:
+    """Wrapper class for the NC command"""
+
     def __init__(
         self,
         connection: Connection,
+        host: Optional[str],
         port: int,
-        host: Optional[str] = None,
-        listen: bool = False, # lflag
-        udp: bool = False, # uflag
-        detached: bool = False, # dflag
-        port_scan: bool = False, # zflag
+        listen: bool = False,  # l-flag
+        udp: bool = False,  # u-flag
+        detached: bool = False,  # d-flag
+        port_scan: bool = False,  # z-flag
     ):
-        flags = "-nv"
+        """
+        Create an instance of the nc command
+
+        Parameters
+        ----------
+            connection : Connection
+                Target connection to run the command on
+            port : int
+                Port number to connect to
+            host : str | None
+                IP address of the host to connect to, nott necessery when the listen option is used
+            listen : bool
+                Listen for an incoming connection rather than initiate a connection to a remote host
+            udp : bool
+                Use UDP instead of TCP protocol
+            detached : bool
+                Do not attempt to read from stdin
+            port_scan : bool
+                Scan for listening daemons, without sending any data
+        """
+        flags = "-nv"  # don't do any dns lookups and enable vebose output
         if listen:
             flags += "l"
         if udp:
@@ -40,59 +62,76 @@ class NetCat:
         self._data_received: asyncio.Event = asyncio.Event()
 
     async def receive_data(self) -> str:
-        await self.data_received()
+        """Receive data from stdout"""
+        await self._data_received.wait()
+        self._data_received.clear()
         data = self._stdout_data
         self._stdout_data = ""
         return data
 
     async def send_data(self, data: str) -> None:
+        """Write data to stdin"""
         await self._process.escape_and_write_stdin([data])
         return None
 
     async def on_stdout(self, stdout: str) -> None:
+        """Handle incoming data"""
         self._stdout_data += stdout
         self._data_received.set()
 
         return None
 
     async def on_stderr(self, stderr: str) -> None:
-        print(datetime.now(), "NetCat stderr:", stderr)
+        """Handle verbose status messages"""
+        print(datetime.now(), "netcat:", stderr)
         await self._output_notifier.handle_output(stderr)
         return None
 
-    async def data_received(self) -> None:
-        await self._data_received.wait()
-        self._data_received.clear()
-
     async def execute(self) -> None:
-        await self._process.execute(stdout_callback=self.on_stdout, stderr_callback=self.on_stderr)
+        await self._process.execute(
+            stdout_callback=self.on_stdout, stderr_callback=self.on_stderr
+        )
 
     @asynccontextmanager
     async def run(self) -> AsyncIterator["NetCat"]:
-        async with self._process.run(stdout_callback=self.on_stdout, stderr_callback=self.on_stderr):
+        async with self._process.run(
+            stdout_callback=self.on_stdout, stderr_callback=self.on_stderr
+        ):
             await self._process.wait_stdin_ready()
             yield self
 
+
 class NetCatServer(NetCat):
+    """Helper class for NC server"""
+
     def __init__(
         self,
         connection: Connection,
         port: int,
         udp: bool = False,
     ):
-        super().__init__(connection, port, listen=True, udp=udp)
+        super().__init__(connection, None, port, listen=True, udp=udp)
         self._listening_event: asyncio.Event = asyncio.Event()
-        self._output_notifier.notify_output("Listening on", self._listening_event)
+        status = "Listening" if not udp else "Bound"
+        self._output_notifier.notify_output(
+            f"{status} on 0.0.0.0 {str(port)}", self._listening_event
+        )
         self._connection_event: asyncio.Event = asyncio.Event()
-        self._output_notifier.notify_output("Connection received on", self._connection_event)
+        self._output_notifier.notify_output(
+            "Connection received on ", self._connection_event
+        )
 
     @asynccontextmanager
     async def run(self) -> AsyncIterator["NetCatServer"]:
-        async with self._process.run(stdout_callback=self.on_stdout, stderr_callback=self.on_stderr):
+        async with self._process.run(
+            stdout_callback=self.on_stdout, stderr_callback=self.on_stderr
+        ):
             await self._process.wait_stdin_ready()
             yield self
 
     async def listening_started(self) -> None:
+        """Wait for listening started event"""
+        # macOS nc does not report any verbose events wiht -l flag
         if platform.system() == "Darwin":
             return None
 
@@ -100,32 +139,52 @@ class NetCatServer(NetCat):
         self._listening_event.clear()
 
     async def connection_received(self) -> None:
+        """Wait for connection received event"""
+        # macOS nc does not report any verbose events wiht -l flag
         if platform.system() == "Darwin":
             return None
 
         await self._connection_event.wait()
         self._connection_event.clear()
 
+
 class NetCatClient(NetCat):
+    """Helper class for NC client"""
+
     def __init__(
         self,
         connection: Connection,
-        port: int,
         host: str,
-        udp: bool = False, # uflag
-        detached: bool = False, # dflag
-        port_scan: bool = False, # zflag
+        port: int,
+        udp: bool = False,
+        detached: bool = False,
+        port_scan: bool = False,
     ):
-        super().__init__(connection, port, host, listen=False, udp=udp, detached=detached, port_scan=port_scan)
+        super().__init__(
+            connection,
+            host,
+            port,
+            listen=False,
+            udp=udp,
+            detached=detached,
+            port_scan=port_scan,
+        )
         self._connection_event: asyncio.Event = asyncio.Event()
-        self._output_notifier.notify_output("Connection to", self._connection_event)
+        protocol = "tcp" if not udp else "udp"
+        self._output_notifier.notify_output(
+            f"Connection to {host} {str(port)} port [{protocol}/*] succeeded!",
+            self._connection_event,
+        )
 
     @asynccontextmanager
     async def run(self) -> AsyncIterator["NetCatClient"]:
-        async with self._process.run(stdout_callback=self.on_stdout, stderr_callback=self.on_stderr):
+        async with self._process.run(
+            stdout_callback=self.on_stdout, stderr_callback=self.on_stderr
+        ):
             await self._process.wait_stdin_ready()
             yield self
 
     async def connection_succeeded(self) -> None:
+        """Wait for connection succeeded event"""
         await self._connection_event.wait()
         self._connection_event.clear()
