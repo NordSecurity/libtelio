f48b48076b9c05d664840d2a07db828b7bb8425b
Luis Cruz <luis.cruz@nordsec.com>
2024-10-01 11:19:28 +0100

    natlab: Support teliod debug binaries

--- nat-lab/tests/test_teliod.py
+++ nat-lab/tests/test_teliod.py
@@ -1,11 +1,12 @@
 import asyncio
 import pytest
+from config import LIBTELIO_BINARY_PATH_DOCKER
 from contextlib import AsyncExitStack
 from helpers import setup_connections
 from utils.connection_util import ConnectionTag
 from utils.process.process import ProcessExecError
 
-TELIOD_EXEC_PATH = "/libtelio/dist/linux/release/x86_64/teliod"
+TELIOD_EXEC_PATH = f"{LIBTELIO_BINARY_PATH_DOCKER}/teliod"
 CONFIG_FILE_PATH = "/etc/teliod/config.json"
 SOCKET_FILE_PATH = "/run/teliod.sock"
 
