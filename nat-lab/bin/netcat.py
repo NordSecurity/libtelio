f6a3f2096cb483a1fcfa7f4252d10357c3397f24
Tomasz Grzegowski <tomasz.grzegowski@nordsec.com>
2024-10-09 12:26:59 +0200

    NC python clone

---
+++ nat-lab/bin/netcat.py
@@ -0,0 +1,217 @@
+import argparse
+from collections.abc import Iterable
+import socket
+import sys
+import select
+import time
+import errno
+from typing import Any
+
+UDP_SCAN_COUNT = 4
+
+# control -v flag
+verbose = False
+
+
+# print a message only in verbose mode
+def vprint(*args, **kwargs):
+    if verbose:
+        print(*args, **kwargs, file=sys.stderr)
+
+
+class NetCat:
+    def __init__(self, args):
+        global verbose
+        verbose = args.v
+        self.args = args
+        self.udp = self.args.u
+        self.sock_type = "udp" if self.udp else "tcp"
+        self.listen = self.args.l
+        self.ipv6 = self.args.ipv6
+        self.sock = self._create_socket()
+        self.client_addr = None
+        self.run()
+
+    def _create_socket(self):
+        """Create a socket based on the protocol and IP version."""
+
+        family = socket.AF_INET6 if self.ipv6 else socket.AF_INET
+        sock_type = socket.SOCK_DGRAM if self.udp else socket.SOCK_STREAM
+
+        try:
+            s = socket.socket(family, sock_type)
+            return s
+        except OSError as e:
+            print(f"error creating socket: {e}")
+            sys.exit(1)
+
+    def _resolve_hostname(self):
+        if self.args.n:
+            try:
+                socket.inet_aton(self.args.hostname)
+                return self.args.hostname
+            except socket.error:
+                print("Error: Hostname is not a valid IP address.")
+                sys.exit(1)
+        else:
+            try:
+                addr_info = socket.getaddrinfo(
+                    self.args.hostname,
+                    None,
+                    socket.AF_INET6 if self.ipv6 else socket.AF_INET,
+                    socket.SOCK_DGRAM if self.udp else socket.SOCK_STREAM,
+                )
+                # getaddrinfo returns a list of tuples, we can extract the first resolved IP address
+                return addr_info[0][4][0]
+            except socket.gaierror as e:
+                print(f"Error resolving hostname: {e}")
+                sys.exit(1)
+
+    def _connect(self):
+        hostname = self._resolve_hostname()
+        port = self.args.port
+
+        try:
+            # Bind to a local port or source address if specified
+            if self.args.p or self.args.s:
+                local_address: str = self.args.s if self.args.s else ""
+                local_port: int = self.args.p if self.args.p else 0
+
+                self.sock.bind((local_address, local_port))
+
+            self.sock.connect((hostname, port))
+            if self.udp:
+                self._udptest()
+            vprint(
+                f"Connection to {hostname} port {port} [{self.sock_type}/*] succeeded!"
+            )
+        except OSError as e:
+            if e.errno == errno.EBADF:
+                # conenction closed
+                sys.exit(0)
+            print(
+                f"nc: connect to {hostname} port {port} ({self.sock_type}) failed: {e.strerror}"
+            )
+            sys.exit(1)
+
+    def _listen(self):
+        # Set SO_REUSEPORT option
+        try:
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        except AttributeError:
+            # SO_REUSEPORT may not be available on all systems, fall back to SO_REUSEADDR
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        port = self.args.port
+        bind_addr = "::" if self.ipv6 else "0.0.0.0"
+        self.sock.bind((bind_addr, port))
+
+        if not self.udp:
+            self.sock.listen(1)
+            vprint(f"Listening on {bind_addr} {port}")
+            conn, addr = self.sock.accept()
+            vprint(f"Connection received on {addr[0]} {addr[1]}")
+            self.sock = conn
+        else:
+            vprint(f"Bound on {bind_addr} {port}")
+
+    def _udptest(self):
+        # Try sending data to the socket
+        self.sock.send(b"X")
+
+        # Wait for the remote host to respond
+        for _ in range(UDP_SCAN_COUNT):
+            time.sleep(1)
+            self.sock.send(b"X")
+
+    def readwrite(self):
+        inputs: Iterable[Any] = [self.sock]
+        if not self.args.d:
+            inputs.append(sys.stdin)
+        while True:
+            readable, _, _ = select.select(inputs, [], [])
+            for r in readable:
+                # read socket
+                if r == self.sock:
+                    data, addr = self.sock.recvfrom(4096)
+                    if self.udp and self.listen and not self.client_addr:
+                        vprint(f"Connection received on {addr[0]} {addr[1]}")
+                        self.client_addr = addr
+                    if not data:
+                        # connection closed
+                        return
+                    sys.stdout.buffer.write(data)
+                    sys.stdout.flush()
+                # read stdin
+                elif r == sys.stdin:
+                    data = sys.stdin.buffer.readline()
+                    if not data:
+                        # EOF on stdin
+                        return
+                    if self.listen and self.client_addr:
+                        self.sock.sendto(data, self.client_addr)
+                    else:
+                        self.sock.send(data)
+
+    def run(self):
+        try:
+            if self.args.l:
+                self._listen()
+            else:
+                self._connect()
+            if not self.args.z:
+                self.readwrite()
+        except KeyboardInterrupt:
+            print("")
+            sys.exit(2)
+        except OSError as e:
+            if e.errno == errno.EBADF:
+                # connection closed
+                sys.exit(0)
+            print(f"nc: {e}")
+            sys.exit(1)
+        finally:
+            self.sock.close()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Netcat clone in Python")
+    parser.add_argument("-6", dest="ipv6", action="store_true", help="Use IPv6")
+    parser.add_argument(
+        "-4", dest="ipv4", action="store_true", help="Use IPv4 addresses"
+    )
+    parser.add_argument("-v", action="store_true", help="Verbose mode")
+    parser.add_argument("-n", action="store_true", help="Do not resolve hostnames")
+    parser.add_argument("-l", action="store_true", help="Listen mode (server)")
+    parser.add_argument("-u", action="store_true", help="UDP mode")
+    parser.add_argument("-d", action="store_true", help="Do not read from stdin")
+    parser.add_argument(
+        "-z", action="store_true", help="Zero-I/O mode [used for scanning]"
+    )
+    parser.add_argument("-p", type=int, help="Specify local port number")
+    parser.add_argument("-s", help="Specify local source address")
+    parser.add_argument("hostname", nargs="?", help="Hostname or IP address")
+    parser.add_argument("port", type=int, help="Port number")
+    args = parser.parse_args()
+
+    if args.z and args.l:
+        parser.error("Cannot use -z in listen mode (-l)")
+    if args.s and args.l:
+        parser.error("Cannot use -s in listen mode (-l)")
+    if args.p and args.l:
+        parser.error("Cannot use -p in listen mode (-l)")
+
+    if not args.port:
+        parser.error("Port required")
+
+    if not args.l:
+        # Connect mode
+        if not args.hostname:
+            parser.error("Hostname required")
+
+    netcat = NetCat(args)
+    netcat.run()
+
+
+if __name__ == "__main__":
+    main()
