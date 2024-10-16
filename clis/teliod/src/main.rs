4ac271fd5672c687e8bcf76b632ee941cba4179e
Tomasz Kłak <tomasz.klak@nordsec.com>
2024-10-16 14:43:42 +0200

    Add various configuration options for mqtt
    Allow for backoff behaviour changes and how often we reconnect to mqtt.

--- clis/teliod/src/main.rs
+++ clis/teliod/src/main.rs
@@ -3,15 +3,17 @@
 use clap::Parser;
 use nix::libc::{SIGHUP, SIGINT, SIGQUIT, SIGTERM};
 use signal_hook_tokio::Signals;
+use smart_default::SmartDefault;
 use std::{
     fs::{self, File},
+    num::NonZeroU64,
     path::PathBuf,
     str::FromStr,
     sync::Arc,
 };
 use thiserror::Error as ThisError;
 use tokio::task::JoinError;
-use tracing::{error, info, level_filters::LevelFilter, warn};
+use tracing::{debug, error, info, level_filters::LevelFilter, warn};
 use uuid::Uuid;
 
 mod comms;
@@ -26,7 +28,55 @@ use serde_json::error::Error as SerdeJsonError;
 
 use futures::stream::StreamExt;
 
-#[derive(Deserialize)]
+#[derive(Clone, Copy, Debug, SmartDefault)]
+#[repr(transparent)]
+struct Percentage(u8);
+
+impl std::ops::Mul<std::time::Duration> for Percentage {
+    type Output = std::time::Duration;
+
+    fn mul(self, rhs: std::time::Duration) -> Self::Output {
+        (self.0 as u32 * rhs) / 100
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, SmartDefault)]
+struct MqttConfig {
+    /// Starting backoff time for mqtt retry, has to be at least one. (in seconds)
+    #[default(backoff_initial_default())]
+    #[serde(default = "backoff_initial_default")]
+    backoff_initial: NonZeroU64,
+    /// Maximum backoff time for the mqtt retry. Has to be greater than the initial value. (in seconds)
+    #[default(backoff_maximal_default())]
+    #[serde(default = "backoff_maximal_default")]
+    backoff_maximal: NonZeroU64,
+
+    /// Percentage of the expiry period after which new mqtt token will be requested
+    #[default(reconnect_after_expiry_default())]
+    #[serde(deserialize_with = "deserialize_percent")]
+    #[serde(default = "reconnect_after_expiry_default")]
+    reconnect_after_expiry: Percentage,
+
+    /// Path to a mqtt pem certificate to be used when connecting to Notification Center
+    #[serde(default)]
+    certificate_file_path: Option<PathBuf>,
+}
+
+fn backoff_initial_default() -> NonZeroU64 {
+    #[allow(unwrap_check)]
+    NonZeroU64::new(1).unwrap()
+}
+
+fn backoff_maximal_default() -> NonZeroU64 {
+    #[allow(unwrap_check)]
+    NonZeroU64::new(300).unwrap()
+}
+
+fn reconnect_after_expiry_default() -> Percentage {
+    Percentage(90)
+}
+
+#[derive(Deserialize, Debug)]
 struct TeliodDaemonConfig {
     #[serde(deserialize_with = "deserialize_log_level")]
     log_level: LevelFilter,
@@ -39,8 +89,22 @@ struct TeliodDaemonConfig {
 
     /// Path to a http pem certificate to be used when connecting to CoreApi
     http_certificate_file_path: Option<PathBuf>,
-    /// Path to a mqtt pem certificate to be used when connecting to Notification Center
-    mqtt_certificate_file_path: Option<PathBuf>,
+
+    #[serde(default)]
+    mqtt: MqttConfig,
+}
+
+fn deserialize_percent<'de, D>(deserializer: D) -> Result<Percentage, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value: u8 = de::Deserialize::deserialize(deserializer)?;
+    if value > 100 {
+        return Err(de::Error::custom(
+            "Percentage value can only be in range from 0 to 100 inclusive",
+        ));
+    }
+    Ok(Percentage(value))
 }
 
 fn deserialize_log_level<'de, D>(deserializer: D) -> Result<LevelFilter, D::Error>
@@ -178,6 +242,8 @@ async fn daemon_event_loop(config: TeliodDaemonConfig) -> Result<(), TeliodError
         .with_level(true)
         .init();
 
+    debug!("started with config: {config:?}");
+
     let socket = DaemonSocket::new(&DaemonSocket::get_ipc_socket_path()?)?;
     let cmd_listener = CommandListener { socket };
 
@@ -240,5 +306,20 @@ async fn daemon_event_loop(config: TeliodDaemonConfig) -> Result<(), TeliodError
         result
     } else {
         join_result.map_err(|err| err.into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use super::*;
+
+    #[test]
+    fn percentage_times_duration() {
+        let input = Duration::from_secs(86400);
+        let expected = Duration::from_secs(77760);
+        let percentage = Percentage(90);
+        assert_eq!(expected, percentage * input);
     }
 }
