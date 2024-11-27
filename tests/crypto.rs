503e6e9ea032c6d6792f9a2527add34a4816b945
Tomasz Klak <tomasz.klak@nordsec.com>
2024-11-27 09:24:09 +0100

    Add release mode tests
    This test verifies that we don't accidently print crypto material stored
    in secret or preshared keys.

---
+++ tests/crypto.rs
@@ -0,0 +1,16 @@
+use telio_crypto::{PresharedKey, SecretKey};
+
+const SK: SecretKey = SecretKey::new([0xBAu8; 32]);
+const PSK: PresharedKey = PresharedKey::new([0xBAu8; 32]);
+
+#[ignore]
+#[test]
+fn secret_key_printing() {
+    // Not marking this whole test as only for release mode, since this can lead to us
+    // running it in debug and getting no failure. This way if you see this test run on
+    // CI (and pass), you know that it was run in the release mode.
+    #[cfg(debug_assertions)]
+    panic!("This test makes no sense in the debug mode, only run it in release mode");
+    assert_eq!("SecretKey(****)", format!("{SK:?}"));
+    assert_eq!("PresharedKey(****)", format!("{PSK:?}"));
+}
