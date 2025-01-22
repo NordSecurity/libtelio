2763318266a914074a38e6259789f2b8f6b72aad
Luis Cruz <luis.cruz@nordsec.com>
2025-01-22 17:04:10 +0000

    qnap: Add app state report

---
+++ clis/teliod/src/cgi/app.rs
@@ -0,0 +1,40 @@
+//! App logic for web ui
+
+use std::collections::HashMap;
+
+use crate::{config::TeliodDaemonConfig, TelioStatusReport};
+
+use super::api::{get_config, get_status_report, is_teliod_running};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub running: bool,
+    pub config: TeliodDaemonConfig,
+    pub status: Option<TelioStatusReport>,
+
+    // TODO: if error occur durring upade_config, this should collect it
+    // and add info to the generated form
+    pub errors: HashMap<String, String>,
+}
+
+impl AppState {
+    /// Collect app state from current enviroment
+    pub fn collect() -> Self {
+        Self {
+            running: is_teliod_running(),
+            config: get_local_or_default_config(),
+            status: get_status_report().ok(),
+            errors: HashMap::new(),
+        }
+    }
+}
+
+pub fn get_local_or_default_config() -> TeliodDaemonConfig {
+    match get_config() {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("Failed to get previous config err: {err}");
+            Default::default()
+        }
+    }
+}
