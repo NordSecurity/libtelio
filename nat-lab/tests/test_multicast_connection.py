284f7c8f76a2ff5308fc0491cc79de09b307f479
Luis Cruz <luis.cruz@nordsec.com>
2025-03-05 14:27:25 +0000

    natlab: Add missing windows pytest mark

--- nat-lab/tests/test_multicast_connection.py
+++ nat-lab/tests/test_multicast_connection.py
@@ -42,6 +42,7 @@ MUILTICAST_TEST_PARAMS = [
             (ConnectionTag.DOCKER_CONE_CLIENT_1, TelioAdapterType.NEP_TUN),
         ]),
         "ssdp",
+        marks=pytest.mark.windows,
     ),
     pytest.param(
         generate_setup_parameter_pair([
@@ -49,6 +50,7 @@ MUILTICAST_TEST_PARAMS = [
             (ConnectionTag.WINDOWS_VM_1, TelioAdapterType.WINDOWS_NATIVE_TUN),
         ]),
         "mdns",
+        marks=pytest.mark.windows,
     ),
     pytest.param(
         generate_setup_parameter_pair([
