6bf57ad9771acf4b4976ac062238beffa787a715
Luis Cruz <luis.cruz@nordsec.com>
2025-03-14 10:10:13 +0000

    natlab: Clean pytest_sessionstart()

--- nat-lab/tests/conftest.py
+++ nat-lab/tests/conftest.py
@@ -389,34 +389,8 @@ def pytest_runtest_setup():
 
 # pylint: disable=unused-argument
 def pytest_sessionstart(session):
-    if os.environ.get("NATLAB_SAVE_LOGS") is None:
-        return
-
-    async def async_context():
-        connections = [
-            await SESSION_SCOPE_EXIT_STACK.enter_async_context(
-                new_connection_raw(gw_tag)
-            )
-            for gw_tag in ConnectionTag
-            if "_GW" in gw_tag.name
-        ]
-
-        connections += [
-            await SESSION_SCOPE_EXIT_STACK.enter_async_context(
-                new_connection_raw(conn_tag)
-            )
-            for conn_tag in [
-                ConnectionTag.DOCKER_DNS_SERVER_1,
-                ConnectionTag.DOCKER_DNS_SERVER_2,
-            ]
-        ]
-
-        await SESSION_SCOPE_EXIT_STACK.enter_async_context(
-            make_tcpdump(connections, session=True)
-        )
-        await SESSION_SCOPE_EXIT_STACK.enter_async_context(make_local_tcpdump())
-
-    RUNNER.run(async_context())
+    if os.environ.get("NATLAB_SAVE_LOGS"):
+        RUNNER.run(start_tcpdump_processes())
 
 
 # pylint: disable=unused-argument
@@ -484,3 +458,24 @@ async def collect_mac_diagnostic_reports():
         log.error(f"Failed to connect to the mac VM: {e}")
         if is_ci:
             raise e
+
+
+async def start_tcpdump_processes():
+    connections = [
+        await SESSION_SCOPE_EXIT_STACK.enter_async_context(new_connection_raw(gw_tag))
+        for gw_tag in ConnectionTag
+        if "_GW" in gw_tag.name and "FULLCONE" not in gw_tag.name
+    ]
+
+    connections += [
+        await SESSION_SCOPE_EXIT_STACK.enter_async_context(new_connection_raw(conn_tag))
+        for conn_tag in [
+            ConnectionTag.DOCKER_DNS_SERVER_1,
+            ConnectionTag.DOCKER_DNS_SERVER_2,
+        ]
+    ]
+
+    await SESSION_SCOPE_EXIT_STACK.enter_async_context(
+        make_tcpdump(connections, session=True)
+    )
+    await SESSION_SCOPE_EXIT_STACK.enter_async_context(make_local_tcpdump())
