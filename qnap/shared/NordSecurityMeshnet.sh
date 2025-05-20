8e8d4fa5a238c9cf0b46b5b4736fbbbabcd69432
Lukas Pukenis <lukas.pukenis@nordsec.com>
2025-05-20 15:54:01 +0300

    Change teliod config file permissions to 0660
    Config file after installing NordSecurityMeshnet
    gets owned by `root` user with 0777 permissions and
    `teliod` is later executed with `admin` user.
    
    Changing the permissions is needed as 0777 is too lax,
    but 0600 doesn't work since a different user accesses it.
    
    As a resut 0660 is chosen
    
    Signed-off-by: Lukas Pukenis <lukas.pukenis@nordsec.com>

--- qnap/shared/NordSecurityMeshnet.sh
+++ qnap/shared/NordSecurityMeshnet.sh
@@ -10,6 +10,9 @@ export QNAP_QPKG=$QPKG_NAME
 TELIOD_CFG_FILE=${QPKG_ROOT}/teliod.cfg
 TELIOD_LOG_FILE="/var/log/teliod.log"
 
+# Change the config file permissions. It contains the auth token so we should prohibit it being read by other users
+chmod 0660 $TELIOD_CFG_FILE
+
 system_log() {
     local log_level
     case "$1" in
