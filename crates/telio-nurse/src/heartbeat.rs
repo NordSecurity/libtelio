66a9d0f21c95bbf95d413156c96e1a674fc7df97
Matas Misiunas <matislovas@gmail.com>
2025-06-04 11:31:48 +0300

    Windows STUN interface bind

--- crates/telio-nurse/src/heartbeat.rs
+++ crates/telio-nurse/src/heartbeat.rs
@@ -1125,6 +1125,8 @@ mod tests {
         fn set_fwmark(&self, _fwmark: u32) {}
 
         fn set_tunnel_interface(&self, _interface: u64) {}
+
+        fn set_ext_if_filter(&self, _list: &[String]) {}
     }
 
     struct State {
