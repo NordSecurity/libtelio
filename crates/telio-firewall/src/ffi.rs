56d3c51ac62188f9828927d0abe2bb662ae67b2b
Michal Ziobro <michal.ziobro@nordsec.com>
2025-06-09 12:08:17 +0300

    [LLT-6314] Implement API functions

--- crates/telio-firewall/src/ffi.rs
+++ crates/telio-firewall/src/ffi.rs
@@ -1,188 +1,32 @@
+//! FFI bindings for the dynamic version of library
+
+use core::slice;
 use std::{
+    convert::TryInto,
     ffi::{c_char, c_void},
-    ptr,
+    ptr::null,
 };
 
-mod conntrack;
-mod instant;
-mod lru_cache;
-mod packet;
+use pnet_packet::{ipv4::Ipv4Packet, ipv6::Ipv6Packet};
+use telio_utils::telio_log_warn;
+
+use crate::{
+    chain::{Chain, LibfwChain, LibfwIpAddr, LibfwIpVersion},
+    conntrack::{unwrap_option_or_return, Conntracker, LibfwDirection, LibfwVerdict},
+    error::LibfwError,
+    firewall::IpPacket,
+};
+
+const LRU_CAPACITY: usize = 4096; // Max entries to keep (sepatately for TCP, UDP, and others)
+const LRU_TIMEOUT: u64 = 120_000; // 2min (https://datatracker.ietf.org/doc/html/rfc4787#section-4.3)
 
 ///
 /// Type for firewall instance
 ///
-pub struct LibfwFirewall {}
-
-///
-/// Possible verdicts for packets.
-///
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-pub enum LibfwVerdict {
-    LibfwVerdictAccept = 0,
-    LibfwVerdictHandleLocally = 1,
-    LibfwVerdictDrop = 2,
-}
-
-///
-/// Conntracker connection states
-///
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-pub enum LibfwConnetionState {
-    LibfwConnectionStateNew = 0,
-    LibfwConnectionStateEstablished = 1,
-}
-
-///
-/// Packet direction
-///
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-pub enum LibfwDirection {
-    LibfwDirectionOutbound = 0,
-    LibfwDirectionInbound = 1,
-}
-
-///
-/// IP type
-///
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-pub enum LibfwIpVersion {
-    LibfwIptypeIpv4 = 0,
-    LibfwIptypeIpv6 = 1,
-}
-
-///
-/// IP data
-///
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub union libfw_ip_data_t {
-    ipv4_bytes: [u8; 4],
-    ipv6_octets: [u8; 16],
-}
-
-///
-/// IP representation
-///
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct LibfwIpAddr {
-    ip_type: LibfwIpVersion,
-    ip_data: libfw_ip_data_t,
-}
-
-///
-/// Filter by subnet and port range
-///
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct LibfwNetworkFilter {
-    network_addr: LibfwIpAddr,
-    network_mask: LibfwIpAddr,
-    port_range_start: u16,
-    port_range_end: u16,
-}
-
-///
-/// Filter by the associate data
-///
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-pub struct LibfwAssociatedData {
-    associated_data: *const u8,
-    associated_data_len: usize,
-}
-
-///
-/// Filter types
-///
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-pub enum LibfwFilterType {
-    LibfwFilterAssociatedData = 0,
-    LibfwFilterConntrackState = 1,
-    LibfwFilterSrcNetwork = 2,
-    LibfwFilterDstNetwork = 3,
-    LibfwFilterDirection = 4,
-}
-
-///
-/// Data for the certain filter type
-///
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub union LibfwFilterData {
-    associated_data_filter: LibfwAssociatedData,
-    conntrack_state_filter: LibfwConnetionState,
-    network_filter: LibfwNetworkFilter,
-    direction_filter: LibfwDirection,
-}
-
-///
-/// Struct describing a single Libfw filter
-///
-#[repr(C)]
-#[derive(Clone)]
-pub struct LibfwFilter {
-    /// Filter may be inverted, meaning, that it is considered
-    /// a match only if filter does *not* match
-    inverted: bool,
-
-    /// Defines a type of filter to match
-    /// the packets against. There are a few
-    /// possible filter types:
-    /// * LIBFW_FILTER_ASSOCIATED_DATA - matches
-    ///   packets based on their associated data.
-    ///   This Normally means public key for NordLynx
-    /// * LIBFW_FILTER_CONNTRACK_STATE - matches
-    ///   connection tracking table state. This can be
-    ///   either "new connection" or "established connection"
-    /// * LIBFW_FILTER_[SRC|DST]_NETWORK - matches against
-    ///   either source or destination network or IP address
-    /// * LIBFW_FILTER_DIRECTION - matches either inbound or
-    ///   outbound packet direction.
-    filter_type: LibfwFilterType,
-
-    /// Contains corresponding filter data
-    filter: LibfwFilterData,
-}
-
-/// A definition of firewall rule it consists of filters which
-/// determine whether rule applies to packet being processed.
-/// And action, which determins what to do with the rule
-#[repr(C)]
-#[derive(Clone)]
-pub struct LibfwRule {
-    /// List of filters all of which match
-    /// in order to consider rule's action
-    filter: *const LibfwFilter,
-
-    /// Length of the filter list
-    filter_count: usize,
-
-    /// Defines an action to be taken when filter
-    /// matches packet. Generally either accept and
-    /// stop processing rule chain or drop and stop
-    /// processing rule chain.
-    action: LibfwVerdict,
-}
-
-/// A chain of rules.
-///
-/// Rules are processed in order specified in the chain.
-/// If _some_ rule in the chain matches and a verdict is
-/// determined - the chain processing terminated.
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-pub struct LibfwChain {
-    /// Chain size
-    rule_count: usize,
-
-    /// List of the rules
-    rules: *const LibfwRule,
+pub struct LibfwFirewall {
+    chain: Option<Chain>,
+    conntracker: Conntracker,
+    alow_ipv6: bool,
 }
 
 ///
@@ -191,23 +35,16 @@ pub struct LibfwChain {
 #[repr(C)]
 #[derive(Copy, Clone, Debug)]
 pub enum LibfwLogLevel {
+    /// Trace level
     LibfwLogLevelTrace = 0,
+    /// Warn level
     LibfwLogLevelDebug = 1,
+    /// Debug level
     LibfwLogLevelInfo = 2,
+    /// Warn level
     LibfwLogLevelWarn = 3,
+    /// Error level
     LibfwLogLevelErr = 4,
-}
-
-///
-/// Possible FW errors
-///
-#[repr(C)]
-#[derive(Copy, Clone, Debug)]
-pub enum LibfwError {
-    LibfwSuccess,
-    LibfwErrorInvalidChain,
-    LibfwErrorNotImplemented,
-    //... Expected to be extended during development
 }
 
 ///
@@ -233,22 +70,9 @@ pub type LibfwLogCallback = Option<extern "C" fn(level: LibfwLogLevel, log_line:
 ///              @ref libfw_init
 /// @param packet - byte array of IP packet
 /// @param packet_len - length in bytes of IP packet in @param packet
-/// @param associated_data - if associated data was passed in inbound
-///                          and outbound packet processing function
-///                          this field will provide the same associated
-///                          data. Inteded to be used as peer identifier
-///                          in case of nordlynx
-/// @param associated_data_len - the length in bytes of associated data
 ///
-pub type LibfwInjectPacketCallback = Option<
-    extern "C" fn(
-        data: *mut LibfwFirewall,
-        packet: *const u8,
-        packet_len: usize,
-        associated_data: *const u8,
-        associated_data_len: usize,
-    ),
->;
+pub type LibfwInjectPacketCallback =
+    Option<extern "C" fn(data: *mut c_void, packet: *const u8, packet_len: usize) -> LibfwError>;
 
 ///
 /// Setup logging
@@ -275,50 +99,136 @@ pub extern "C" fn libfw_set_log_callback(
 ///
 #[no_mangle]
 pub extern "C" fn libfw_init() -> *mut LibfwFirewall {
-    ptr::null_mut()
+    Box::into_raw(Box::new(LibfwFirewall {
+        chain: None,
+        conntracker: Conntracker::new(LRU_CAPACITY, LRU_TIMEOUT),
+        alow_ipv6: false,
+    }))
 }
 
 ///
-/// Configures chain of rules for the firewall to follow
+/// Configures chain of rules for the firewall to follow.
+///
+/// The provided chain will be copied and won't be modified in any way. The library
+/// won't try to deallocate it, so freeing the memory after using this chain is the
+/// user's responsibility.
 ///
 /// @param fw - pointer returned by @ref libfw_init
 /// @param chain - chain of the firewall rules
 ///
+/// # Safety
+///
+/// This function dereferences pointer to firewall - user must ensure that this is
+/// the pointer returned by `libfw_init`.
 #[no_mangle]
-pub extern "C" fn libfw_configure_chain(_fw: *mut LibfwFirewall, _chain: LibfwChain) -> LibfwError {
-    LibfwError::LibfwErrorNotImplemented
+pub unsafe extern "C" fn libfw_configure_chain(
+    firewall: *mut LibfwFirewall,
+    chain: LibfwChain,
+) -> LibfwError {
+    // TODO: Check if the chain is valid
+    match (&chain).try_into() {
+        Ok(chain) => {
+            (*firewall).chain = Some(chain);
+            LibfwError::LibfwSuccess
+        }
+        Err(err) => err,
+    }
 }
 
 ///
-/// Retrieves currently configured chain of firewall rules
+/// Retrieves currently configured chain of firewall rules.
+///
+/// The returned chain should be used only as read-only and should be freed only using
+/// `libfw_cleanup_dumped_chain` - it might not be compatible with your allocator and thus
+/// trying to deallocate it in any other way may lead to severe memory problems.
 ///
 /// @param fw - pointer to initialized firewall
-/// @param output - output parameter, returns currenlty configured chain
+/// @param output - output parameter, returns currently configured chain
+///
+/// # Safety
+///
+/// This function dereferences pointer to firewall - user must ensure that this is
+/// the pointer returned by `libfw_init`.
+#[no_mangle]
+pub unsafe extern "C" fn libfw_dump_chain(firewall: *mut LibfwFirewall) -> *const LibfwChain {
+    if firewall.is_null() {
+        return null();
+    }
+
+    if let Some(chain) = unsafe { &(*firewall).chain } {
+        Box::into_raw(Box::new(chain.into()))
+    } else {
+        null()
+    }
+}
+
+///
+/// Frees chain dumped with `libfw_dump_chain` (it shouldn't be used on any other chain instances, though!)
+///
+/// @param chain - chain to be freed
+///
+/// # Safety
+///
+/// Dereferences a raw pointer to the chain, assuming that the provided chain is obtained with
+/// calling `libfw_dump_chain` function - calling it on user-allocated chain, or any other pointer
+/// causes undefined behavior.
 ///
 #[no_mangle]
-pub extern "C" fn libfw_dump_chain(
-    _fw: *mut LibfwFirewall,
-    _output: *const LibfwChain,
-) -> LibfwError {
-    LibfwError::LibfwErrorNotImplemented
+pub unsafe extern "C" fn libfw_cleanup_dumped_chain(chain: *const LibfwChain) {
+    if chain.is_null() {
+        return;
+    }
+
+    let _ = Box::from_raw(chain as *mut LibfwChain);
 }
 
 ///
 /// A function which triggers stale connection closing
 ///
 /// @param fw - pointer returned by @ref libfw_init
+/// @param associated_ip -
+/// @param associated_data - data associated with connections to be closed,
+///                          typically peer public key
+/// @param associated_data_len - len of the associated data
 /// @param inject_packet_cb_data - a pointer which will be passed in the
 ///                                inject_packet callback unmodified
 /// @param inject_packet_cb - callback which will be used by libfw to inject
 ///                           packets into virtual tunnel interface
 ///
+/// # Safety
+///
+/// This function dereferences pointer to firewall - user must ensure that this is
+/// the pointer returned by `libfw_init`.
 #[no_mangle]
-pub extern "C" fn libfw_trigger_stale_connection_close(
-    _fw: *mut LibfwFirewall,
-    _inject_packet_cb_data: *mut c_void,
-    _inject_packet_cb: LibfwInjectPacketCallback,
+pub unsafe extern "C" fn libfw_trigger_stale_connection_close(
+    firewall: *mut LibfwFirewall,
+    associated_ip: LibfwIpAddr,
+    associated_data: *const u8,
+    associated_data_len: usize,
+    inject_packet_cb_data: *mut c_void,
+    inject_packet_cb: LibfwInjectPacketCallback,
 ) -> LibfwError {
-    LibfwError::LibfwErrorNotImplemented
+    if let Some(callback) = inject_packet_cb {
+        if let LibfwIpVersion::LibfwIptypeIpv4 = associated_ip.ip_version {
+            let assoc_data = slice::from_raw_parts(associated_data, associated_data_len);
+            let conntrack = &(*firewall).conntracker;
+            if let Err(err) = conntrack.reset_tcp_conns(assoc_data, |packet| {
+                callback(inject_packet_cb_data, packet.as_ptr(), packet.len()).into()
+            }) {
+                return Err(err).into();
+            }
+            if let Err(err) = conntrack.reset_udp_conns(assoc_data, |packet| {
+                callback(inject_packet_cb_data, packet.as_ptr(), packet.len()).into()
+            }) {
+                return Err(err).into();
+            }
+            LibfwError::LibfwSuccess
+        } else {
+            LibfwError::LibfwErrorNotImplemented
+        }
+    } else {
+        LibfwError::LibfwErrorNullPointer
+    }
 }
 
 ///
@@ -334,15 +244,71 @@ pub extern "C" fn libfw_trigger_stale_connection_close(
 /// @return - returns LIBFW_VERDICT, integrators should allow packet to go through if
 ///           and only if function returns LIBFW_VERDICT_ACCEPT
 ///
+/// # Safety
+///
+/// This function dereferences pointer to firewall - user must ensure that this is
+/// the pointer returned by `libfw_init` - and also `packet` and `associated_data`
+/// which must be allocated with valid lengts (`packet_len` and `associated_data_len`,
+/// respectively).
 #[no_mangle]
-pub extern "C" fn libfw_process_inbound_packet(
-    _fw: *mut LibfwFirewall,
-    _packet: *mut u8,
-    _packet_len: usize,
-    _associated_data: *mut u8,
-    _associated_data_len: usize,
+pub unsafe extern "C" fn libfw_process_inbound_packet(
+    firewall: *mut LibfwFirewall,
+    packet: *mut u8,
+    packet_len: usize,
+    associated_data: *mut u8,
+    associated_data_len: usize,
 ) -> LibfwVerdict {
-    LibfwVerdict::LibfwVerdictAccept
+    let packet_buffer = unsafe { slice::from_raw_parts(packet, packet_len) };
+    let assoc_data = unsafe { slice::from_raw_parts(associated_data, associated_data_len) };
+
+    match unwrap_option_or_return!(packet_buffer.first(), LibfwVerdict::LibfwVerdictDrop) >> 4 {
+        4 => {
+            let ip = unwrap_option_or_return!(
+                Ipv4Packet::try_from(packet_buffer),
+                LibfwVerdict::LibfwVerdictDrop
+            );
+            let verdict = if let Some(chain) = &(*firewall).chain {
+                chain.process_packet(
+                    unsafe { &(*firewall).conntracker },
+                    &ip,
+                    assoc_data,
+                    LibfwDirection::LibfwDirectionInbound,
+                )
+            } else {
+                LibfwVerdict::LibfwVerdictAccept
+            };
+
+            unsafe { &(*firewall).conntracker }.handle_inbound_packet(&ip, assoc_data, verdict);
+
+            verdict
+        }
+        6 if (*firewall).alow_ipv6 => {
+            let ip = unwrap_option_or_return!(
+                Ipv6Packet::try_from(packet_buffer),
+                LibfwVerdict::LibfwVerdictDrop
+            );
+            let verdict = if let Some(chain) = unsafe { &(*firewall).chain } {
+                chain.process_packet(
+                    unsafe { &(*firewall).conntracker },
+                    &ip,
+                    assoc_data,
+                    LibfwDirection::LibfwDirectionInbound,
+                )
+            } else {
+                LibfwVerdict::LibfwVerdictAccept
+            };
+
+            (*firewall)
+                .conntracker
+                .handle_inbound_packet(&ip, assoc_data, verdict);
+
+            verdict
+        }
+        version => {
+            telio_log_warn!("Unexpected IP version {version} for outbound packet");
+            LibfwVerdict::LibfwVerdictDrop
+        }
+    }
 }
 
 ///
@@ -358,24 +324,82 @@ pub extern "C" fn libfw_process_inbound_packet(
 /// @return - returns LIBFW_VERDICT, integrators should allow packet to go through if
 ///           and only if function retruns LIBFW_VERDICT_ACCEPT
 ///
+/// # Safety
+///
+/// This function dereferences pointer to firewall - user must ensure that this is
+/// the pointer returned by `libfw_init` - and also `packet` and `associated_data`
+/// which must be allocated with valid lengts (`packet_len` and `associated_data_len`,
+/// respectively).
 #[no_mangle]
-pub extern "C" fn libfw_process_outbound_packet(
-    _fw: *mut LibfwFirewall,
-    _packet: *const u8,
-    _packet_len: usize,
-    _associated_data: *const u8,
-    _associated_data_len: usize,
+pub unsafe extern "C" fn libfw_process_outbound_packet(
+    firewall: *mut LibfwFirewall,
+    packet: *const u8,
+    packet_len: usize,
+    associated_data: *const u8,
+    associated_data_len: usize,
 ) -> LibfwVerdict {
-    LibfwVerdict::LibfwVerdictAccept
+    let packet_buffer = slice::from_raw_parts(packet, packet_len);
+    let assoc_data = slice::from_raw_parts(associated_data, associated_data_len);
+
+    match unwrap_option_or_return!(packet_buffer.first(), LibfwVerdict::LibfwVerdictDrop) >> 4 {
+        4 => {
+            let ip = unwrap_option_or_return!(
+                Ipv4Packet::try_from(packet_buffer),
+                LibfwVerdict::LibfwVerdictDrop
+            );
+            unsafe { &(*firewall).conntracker }.handle_outbound_packet(&ip, assoc_data);
+            if let Some(chain) = &(*firewall).chain {
+                chain.process_packet(
+                    &(*firewall).conntracker,
+                    &ip,
+                    assoc_data,
+                    LibfwDirection::LibfwDirectionOutbound,
+                )
+            } else {
+                LibfwVerdict::LibfwVerdictAccept
+            }
+        }
+        6 if unsafe { (*firewall).alow_ipv6 } => {
+            let ip = unwrap_option_or_return!(
+                Ipv6Packet::try_from(packet_buffer),
+                LibfwVerdict::LibfwVerdictDrop
+            );
+            unsafe { &(*firewall).conntracker }.handle_outbound_packet(&ip, assoc_data);
+            if let Some(chain) = &(*firewall).chain {
+                chain.process_packet(
+                    &(*firewall).conntracker,
+                    &ip,
+                    assoc_data,
+                    LibfwDirection::LibfwDirectionOutbound,
+                )
+            } else {
+                LibfwVerdict::LibfwVerdictAccept
+            }
+        }
+        version => {
+            telio_log_warn!("Unexpected IP version {version} for outbound packet");
+            LibfwVerdict::LibfwVerdictDrop
+        }
+    }
 }
 
 ///
 /// Destructs firewall instance.
 ///
-/// After this function returns it is guaranteed that
-/// no callbacks will be called anymore
+/// After this function returns it is guaranteed that no callbacks will
+/// be called anymore.
 ///
 /// @param fw - pointer returned by @ref libfw_init
 ///
+/// # Safety
+///
+/// This function dereferences pointer to firewall - user must ensure that this is
+/// the pointer returned by `libfw_init`.
 #[no_mangle]
-pub extern "C" fn libfw_deinit(_fw: *mut LibfwFirewall) {}
+pub unsafe extern "C" fn libfw_deinit(firewall: *mut LibfwFirewall) {
+    if !firewall.is_null() {
+        unsafe {
+            let _ = Box::from_raw(firewall);
+        }
+    }
+}
