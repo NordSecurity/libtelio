e161c08534450cdb377ba9d0fc0ccaef375989c5
Luis Cruz <luis.cruz@nordsec.com>
2025-06-11 18:10:12 +0100

    teliod: Compute qpkg install path at runtime

--- clis/teliod/src/cgi/constants.rs
+++ clis/teliod/src/cgi/constants.rs
@@ -1,12 +1,46 @@
 use const_format::concatcp;
+use std::path::PathBuf;
+use std::process::Command;
+use std::sync::LazyLock;
 
 #[cfg(feature = "qnap")]
-pub const APP_DIR: &str = "/share/CACHEDEV1_DATA/.qpkg/NordSecurityMeshnet";
+pub static APP_DIR: LazyLock<Option<PathBuf>> = LazyLock::new(|| {
+    let output = Command::new("getcfg")
+        .args([
+            "NordSecurityMeshnet",
+            "Install_Path",
+            "-f",
+            "/etc/config/qpkg.conf",
+        ])
+        .output()
+        .ok()?;
+
+    if !output.status.success() {
+        None
+    } else {
+        let stdout = String::from_utf8(output.stdout).ok()?;
+        let trimmed = stdout.trim();
+
+        if trimmed.is_empty() {
+            None
+        } else {
+            Some(PathBuf::from(trimmed))
+        }
+    }
+});
+// pub static APP_DIR: LazyLock<PathBuf> = LazyLock::new(|| {
+//     std::env::var("NORDSECURITY_MESHNET_APP_DIR")
+//         .map(PathBuf::from)
+//         .unwrap_or_else(|_| PathBuf::from("/share/CACHEDEV1_DATA/.qpkg/NordSecurityMeshnet"))
+// });
 #[cfg(not(feature = "qnap"))]
 pub const APP_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../../target/debug");
 
-pub const TELIOD_BIN: &str = concatcp!(APP_DIR, "/teliod");
-pub const TELIOD_CFG: &str = concatcp!(APP_DIR, "/teliod.cfg");
+pub static TELIOD_BIN: LazyLock<Option<PathBuf>> =
+    LazyLock::new(|| APP_DIR.as_ref().map(|dir| dir.join("teliod")));
+
+pub static TELIOD_CFG: LazyLock<Option<PathBuf>> =
+    LazyLock::new(|| APP_DIR.as_ref().map(|dir| dir.join("teliod.cfg")));
 
 /// Base directory for the rotating libtelio trace logs
 pub const TELIOD_LIB_LOG_DIR: &str = "/var/log";
@@ -20,7 +54,8 @@ pub const TELIOD_STDOUT_LOG: &str = "/var/log/teliod.log";
 pub const TELIOD_INIT_LOG: &str = "/var/log/teliod_init.log";
 
 #[cfg(debug_assertions)]
-pub const CGI_LOG: &str = concatcp!(APP_DIR, "/cgi.log");
+pub static CGI_LOG: LazyLock<Option<PathBuf>> =
+    LazyLock::new(|| APP_DIR.as_ref().map(|dir| dir.join("cgi.log")));
 
 /// Path to the user intent file used to automatically turn meshnet on if found by NordSecurityMeshnet.sh
 pub const TELIOD_START_INTENT_FILE: &str = concatcp!(APP_DIR, "/.teliod_start_intent");
