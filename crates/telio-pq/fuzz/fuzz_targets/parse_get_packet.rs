40158501703c5299a736de8365626aec8da734a9
Tomasz Kłak <tomasz.klak@nordsec.com>
2025-06-20 12:03:04 +0200

    Implement RFC LLT-0101
    This add v2 of the post-quantum protocol that prevents local DoS via the
    rekey endpoint.

--- crates/telio-pq/fuzz/fuzz_targets/parse_get_packet.rs
+++ crates/telio-pq/fuzz/fuzz_targets/parse_get_packet.rs
@@ -3,5 +3,6 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _result = telio_pq::fuzz::parse_get_response(data, 49152);
+    let _result = telio_pq::fuzz::parse_get_response(data, 49152, 1);
+    let _result = telio_pq::fuzz::parse_get_response(data, 49152, 2);
 });
