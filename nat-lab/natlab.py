ae87f5d2f9fe0d75b15975545525644b1866846b
Oleksandr Oliinyk <oleksandr.oliinyk@nordsec.com>
2025-07-30 12:48:24 +0200

    Add openwrt container to nat-lab

--- nat-lab/natlab.py
+++ nat-lab/natlab.py
@@ -102,6 +102,8 @@ def check_containers() -> None:
     docker_status = run_command_with_output(
         ["docker", "ps", "--filter", "status=running"]
     )
+    openwrt_container_log = run_command_with_output(["docker", "compose", "logs", "nat-lab-openwrt-gw-01-1"])
+    print(openwrt_container_log)
     docker_status = [line.strip() for line in docker_status.splitlines()]
 
     missing_services: List[str] = []
