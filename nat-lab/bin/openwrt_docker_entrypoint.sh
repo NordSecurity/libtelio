0fb84c55a4138d8c5d406a86dd77e453e46a2654
Oleksandr Oliinyk <oleksandr.oliinyk@nordsec.com>
2025-08-04 11:10:35 +0200

    Add openwrt container to nat-lab

--- nat-lab/bin/openwrt_docker_entrypoint.sh
+++ nat-lab/bin/openwrt_docker_entrypoint.sh
@@ -8,7 +8,6 @@ RUN ls -l /opt/bin
 echo "IPK file debug info:"
 ls -lah /opt/bin/*.ipk
 md5sum /opt/bin/*.ipk
-ar t /opt/bin/*.ipk || echo "ar extraction failed"
 head -c 256 /opt/bin/*.ipk | hexdump -C
 
 opkg --version
