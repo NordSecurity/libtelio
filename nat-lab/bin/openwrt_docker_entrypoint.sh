c81621e7296213dca012cff8ca030bb6a33e83a9
Oleksandr Oliinyk <oleksandr.oliinyk@nordsec.com>
2025-08-04 14:09:40 +0200

    Add openwrt container to nat-lab

--- nat-lab/bin/openwrt_docker_entrypoint.sh
+++ nat-lab/bin/openwrt_docker_entrypoint.sh
@@ -13,6 +13,6 @@ head -c 256 /opt/bin/*.ipk | hexdump -C
 opkg --version
 uname -a
 
-opkg install /opt/bin/rpcd-mod-wireguard_1_x86_64.ipk -V4
+opkg install /opt/bin/nordvpn_*_x86_64.ipk -V4
 
 exec sleep infinity
