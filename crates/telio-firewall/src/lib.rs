00dfacbdcb8b6f5320243e31faafc5d4eff8ee51
Michal Ziobro <michal.ziobro@nordsec.com>
2025-09-26 18:06:14 +0300

    Add necessary changes to telio-firewall files

--- crates/telio-firewall/src/lib.rs
+++ crates/telio-firewall/src/lib.rs
@@ -6,6 +6,9 @@
 pub(crate) mod chain;
 pub(crate) mod chain_helpers;
 pub(crate) mod conntrack;
+pub(crate) mod error;
 pub(crate) mod ffi_chain;
 pub mod firewall;
+pub(crate) mod libfirewall_api;
+pub(crate) mod log;
 pub(crate) mod packet;
