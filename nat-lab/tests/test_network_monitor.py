3933e949927e50a325c62e847506e4ab0b41448b
Luis Cruz <luis.cruz@nordsec.com>
2025-10-06 12:51:04 +0100

    Improve network-monitors logging

--- nat-lab/tests/test_network_monitor.py
+++ nat-lab/tests/test_network_monitor.py
@@ -32,11 +32,17 @@ DEFAULT_WAITING_TIME = 2
 async def test_network_monitor(
     alpha_setup_params: SetupParameters,
 ) -> None:
+    # 1 [interface creation] + 1 [set IP] + 1 [remove IP] + 1 [set IP] -> interface initialization + restart
+    # TODO: This value might differ for macOS (LLT-6728)
+    NR_OF_NOTIFICATIONS = 4
     async with AsyncExitStack() as exit_stack:
         env = await setup_mesh_nodes(exit_stack, [alpha_setup_params])
         [client_alpha] = env.clients
 
         await asyncio.sleep(DEFAULT_WAITING_TIME)
         await client_alpha.restart_interface()
-
-        await client_alpha.wait_for_log("Updating local addr cache")
+        await client_alpha.wait_for_log(
+            "Detected network interface modification, notifying..",
+            count=NR_OF_NOTIFICATIONS,
+            not_greater=True,
+        )
