511be216f08bab5b0ee349efc284a3094f0bfee5
Tomasz Grzegowski <tomasz.grzegowski@nordsec.com>
2026-03-12 13:51:41 +0100

    Forwarder unit tests

--- crates/telio-dns/src/forwarder.rs
+++ crates/telio-dns/src/forwarder.rs
@@ -196,6 +196,38 @@ impl RawForwarder {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tokio::task::JoinHandle;
+
+    enum StubBehavior {
+        Reply(Vec<u8>),
+        DelayedReply { delay: Duration, response: Vec<u8> },
+        BlackHole,
+    }
+
+    async fn spawn_stub(behavior: StubBehavior) -> (SocketAddr, JoinHandle<Vec<u8>>) {
+        let socket = UdpSocket::bind("127.0.0.1:0").await.unwrap();
+        let addr = socket.local_addr().unwrap();
+
+        let handle = tokio::spawn(async move {
+            let mut buf = vec![0u8; 4096];
+            let (n, src) = socket.recv_from(&mut buf).await.unwrap();
+            let received = buf[..n].to_vec();
+
+            match behavior {
+                StubBehavior::Reply(response) => {
+                    socket.send_to(&response, src).await.unwrap();
+                }
+                StubBehavior::DelayedReply { delay, response } => {
+                    tokio::time::sleep(delay).await;
+                    socket.send_to(&response, src).await.unwrap();
+                }
+                StubBehavior::BlackHole => {}
+            }
+            received
+        });
+
+        (addr, handle)
+    }
 
     #[tokio::test]
     async fn set_upstreams_stores_resolvers() {
@@ -246,17 +278,6 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn cloned_forwarders_share_channel() {
-        let forwarder1 = RawForwarder::new().await.unwrap();
-        let forwarder2 = forwarder1.clone();
-
-        let upstreams = vec!["8.8.8.8:53".parse().unwrap()];
-        forwarder1.set_upstreams(upstreams).await.unwrap();
-
-        assert!(Arc::ptr_eq(&forwarder1.upstreams, &forwarder2.upstreams),);
-    }
-
-    #[tokio::test]
     async fn set_timeout_updates_value() {
         let forwarder = RawForwarder::new().await.unwrap();
         let target_timeout = Duration::from_secs(10);
@@ -288,5 +309,123 @@ mod tests {
                 other
             ),
         }
+    }
+
+    #[tokio::test]
+    async fn forward_query_returns_response_from_first_upstream() {
+        let expected_response = b"dns-response-bytes".to_vec();
+        let (addr, _handle) = spawn_stub(StubBehavior::Reply(expected_response.clone())).await;
+
+        let forwarder = RawForwarder::new().await.unwrap();
+        forwarder.set_upstreams(vec![addr]).await.unwrap();
+
+        let result = forwarder.query(b"dns-query").await.unwrap();
+
+        assert_eq!(result, expected_response);
+    }
+
+    #[tokio::test]
+    async fn forward_query_falls_back_to_second_upstream() {
+        let (blackhole_addr, _bh_handle) = spawn_stub(StubBehavior::BlackHole).await;
+        let expected = b"from-second-upstream".to_vec();
+        let (reply_addr, _reply_handle) = spawn_stub(StubBehavior::Reply(expected.clone())).await;
+
+        let forwarder = RawForwarder::new().await.unwrap();
+        forwarder
+            .set_upstreams(vec![blackhole_addr, reply_addr])
+            .await
+            .unwrap();
+        forwarder.set_timeout(Duration::from_millis(50)).await;
+
+        let result = forwarder.query(b"query").await.unwrap();
+
+        assert_eq!(result, expected);
+    }
+
+    #[tokio::test]
+    async fn forward_query_tries_upstreams_in_order() {
+        let first_response = b"first-upstream".to_vec();
+        let second_response = b"second-upstream".to_vec();
+        let (first_addr, _first_handle) =
+            spawn_stub(StubBehavior::Reply(first_response.clone())).await;
+        let (second_addr, _second_handle) = spawn_stub(StubBehavior::Reply(second_response)).await;
+
+        let forwarder = RawForwarder::new().await.unwrap();
+        forwarder
+            .set_upstreams(vec![first_addr, second_addr])
+            .await
+            .unwrap();
+        forwarder.set_timeout(Duration::from_secs(2)).await;
+
+        let result = forwarder.query(b"query").await.unwrap();
+
+        assert_eq!(result, first_response);
+    }
+
+    #[tokio::test]
+    async fn forward_query_passes_through_large_response() {
+        let large = vec![0xAB; FORWARDER_BUFFER_SIZE - 1];
+        let (addr, _handle) = spawn_stub(StubBehavior::Reply(large.clone())).await;
+
+        let forwarder = RawForwarder::new().await.unwrap();
+        forwarder.set_upstreams(vec![addr]).await.unwrap();
+        forwarder.set_timeout(Duration::from_secs(2)).await;
+
+        let result = forwarder.query(b"query").await.unwrap();
+
+        assert_eq!(result, large);
+    }
+
+    #[tokio::test]
+    async fn forward_query_serializes_concurrent_requests() {
+        let response = b"concurrent-response".to_vec();
+        let resp_clone = response.clone();
+
+        let stub_socket = UdpSocket::bind("127.0.0.1:0").await.unwrap();
+        let stub_addr = stub_socket.local_addr().unwrap();
+
+        let stub_handle = tokio::spawn(async move {
+            let mut buf = vec![0u8; 4096];
+            for _ in 0..2 {
+                let (n, src) = stub_socket.recv_from(&mut buf).await.unwrap();
+                tokio::time::sleep(Duration::from_millis(50)).await;
+                stub_socket.send_to(&resp_clone, src).await.unwrap();
+                let _ = &buf[..n];
+            }
+        });
+
+        let forwarder = RawForwarder::new().await.unwrap();
+        forwarder.set_upstreams(vec![stub_addr]).await.unwrap();
+
+        let f1 = forwarder.clone();
+        let f2 = forwarder.clone();
+
+        let query1 = tokio::spawn(async move { f1.query(b"query1").await });
+        let query2 = tokio::spawn(async move { f2.query(b"query2").await });
+
+        let (r1, r2) = tokio::join!(query1, query2);
+        assert_eq!(r1.unwrap().unwrap(), response);
+        assert_eq!(r2.unwrap().unwrap(), response);
+
+        stub_handle.await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn forward_query_works_after_upstream_change() {
+        let first_response = b"first-server".to_vec();
+        let second_response = b"second-server".to_vec();
+        let (first_addr, _h1) = spawn_stub(StubBehavior::Reply(first_response.clone())).await;
+        let (second_addr, _h2) = spawn_stub(StubBehavior::Reply(second_response.clone())).await;
+
+        let forwarder = RawForwarder::new().await.unwrap();
+        forwarder.set_upstreams(vec![first_addr]).await.unwrap();
+
+        let r1 = forwarder.query(b"query").await.unwrap();
+        assert_eq!(r1, first_response);
+
+        forwarder.set_upstreams(vec![second_addr]).await.unwrap();
+
+        let r2 = forwarder.query(b"query").await.unwrap();
+        assert_eq!(r2, second_response);
     }
 }
