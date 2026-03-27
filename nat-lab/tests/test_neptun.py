a8d9ce188f2dad0a52ffe79ee222949e5e1c69f8
Gytis Stoškevičius <gytisstoskevicius@gmail.com>
2026-03-27 10:21:23 +0200

    refactor(nat-lab): apply setup-phase fixtures to test_neptun.py
    Convert test_neptun.py to use setup-phase env/env_mesh fixtures with
    compositional vpn_tags and autouse mutation fixtures instead of
    factory-pattern runtime setup.

--- nat-lab/tests/test_neptun.py
+++ nat-lab/tests/test_neptun.py
@@ -1,32 +1,37 @@
 import pytest
-from contextlib import AsyncExitStack
-from tests.helpers import setup_mesh_nodes, SetupParameters, ping_between_all_nodes
+from tests.helpers import SetupParameters, Environment, ping_between_all_nodes
 from tests.utils.bindings import default_features, TelioAdapterType
 from tests.utils.connection import ConnectionTag
+
+pytest_plugins = ["tests.helpers_fixtures"]
 
 BUFFER_SIZE = 1310720
 
 
+@pytest.mark.parametrize(
+    "alpha_setup_params, beta_setup_params",
+    [
+        pytest.param(
+            SetupParameters(
+                connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_1,
+                adapter_type_override=TelioAdapterType.NEP_TUN,
+                features=default_features(custom_skt_buffer_size=BUFFER_SIZE),
+            ),
+            SetupParameters(
+                connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_2,
+            ),
+        ),
+    ],
+)
 @pytest.mark.asyncio
-async def test_custom_socket_buffers_on_neptun() -> None:
-    async with AsyncExitStack() as exit_stack:
-        env = await setup_mesh_nodes(
-            exit_stack,
-            [
-                SetupParameters(
-                    connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_1,
-                    adapter_type_override=TelioAdapterType.NEP_TUN,
-                    features=default_features(custom_skt_buffer_size=BUFFER_SIZE),
-                ),
-                SetupParameters(
-                    connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_2,
-                ),
-            ],
-        )
-
-        [client_alpha, _] = env.clients
-        await client_alpha.wait_for_log(
-            'Socket buffer "RcvBuf" set with value ' + str(BUFFER_SIZE)
-        )
-        # Ping to check connection works both ways
-        await ping_between_all_nodes(env)
+async def test_custom_socket_buffers_on_neptun(
+    alpha_setup_params: SetupParameters,  # pylint: disable=unused-argument
+    beta_setup_params: SetupParameters,  # pylint: disable=unused-argument
+    env_mesh: Environment,
+) -> None:
+    client_alpha = env_mesh.clients[0]
+    await client_alpha.wait_for_log(
+        'Socket buffer "RcvBuf" set with value ' + str(BUFFER_SIZE)
+    )
+    # Ping to check connection works both ways
+    await ping_between_all_nodes(env_mesh)
