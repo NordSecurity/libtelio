3b28de224cf62ff03546f3d62bf5a5e372ab2766
Gytis Stoškevičius <gytisstoskevicius@gmail.com>
2026-03-27 10:21:39 +0200

    refactor(nat-lab): apply setup-phase fixtures to test_proxy.py
    Convert test_proxy.py to use setup-phase env/env_mesh fixtures with
    compositional vpn_tags and autouse mutation fixtures instead of
    factory-pattern runtime setup.

--- nat-lab/tests/test_proxy.py
+++ nat-lab/tests/test_proxy.py
@@ -1,49 +1,62 @@
 import asyncio
 import pytest
 from contextlib import AsyncExitStack
-from tests.helpers import SetupParameters, setup_mesh_nodes
+from tests.helpers import SetupParameters, Environment
 from tests.utils.bindings import TelioNode
 from tests.utils.connection import ConnectionTag
 from tests.utils.ping import ping
 from typing import Optional
 
+pytest_plugins = ["tests.helpers_fixtures"]
 
+
+@pytest.mark.parametrize(
+    "alpha_setup_params, beta_setup_params",
+    [
+        pytest.param(
+            SetupParameters(connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_1),
+            SetupParameters(connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_2),
+        ),
+    ],
+)
 @pytest.mark.asyncio
-async def test_proxy_endpoint_map_update() -> None:
-    setup_params = [
-        SetupParameters(connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_1),
-        SetupParameters(connection_tag=ConnectionTag.DOCKER_CONE_CLIENT_2),
-    ]
+async def test_proxy_endpoint_map_update(
+    alpha_setup_params: SetupParameters,  # pylint: disable=unused-argument
+    beta_setup_params: SetupParameters,  # pylint: disable=unused-argument
+    exit_stack: AsyncExitStack,
+    env_mesh: Environment,
+) -> None:
+    alpha_node, beta_node = env_mesh.nodes[0], env_mesh.nodes[1]
+    alpha_client = env_mesh.clients[0]
+    alpha_conn, beta_conn = (
+        env_mesh.connections[0].connection,
+        env_mesh.connections[1].connection,
+    )
 
-    async with AsyncExitStack() as exit_stack:
-        env = await setup_mesh_nodes(exit_stack, setup_params)
-        alpha, beta = env.nodes
-        alpha_connection, beta_connection = [
-            conn.connection for conn in env.connections
-        ]
-        alpha_client, _ = env.clients
+    alpha = alpha_node
+    beta = beta_node
+    alpha_connection = alpha_conn
+    beta_connection = beta_conn
 
-        port = node_port(alpha_client.get_node_state(beta.public_key))
+    port = node_port(alpha_client.get_node_state(beta.public_key))
 
-        await exit_stack.enter_async_context(
-            alpha_client.get_router().block_udp_port(port)
-        )
+    await exit_stack.enter_async_context(alpha_client.get_router().block_udp_port(port))
 
-        await ping(alpha_connection, beta.ip_addresses[0])
-        await ping(beta_connection, alpha.ip_addresses[0])
+    await ping(alpha_connection, beta.ip_addresses[0])
+    await ping(beta_connection, alpha.ip_addresses[0])
 
-        for _ in range(0, 5):
-            new_port = node_port(alpha_client.get_node_state(beta.public_key))
-            if port != new_port:
-                break
-            await asyncio.sleep(1)
-        else:
-            assert False, "Endpoint wasn't successfully updated"
+    for _ in range(0, 5):
+        new_port = node_port(alpha_client.get_node_state(beta.public_key))
+        if port != new_port:
+            break
+        await asyncio.sleep(1)
+    else:
+        assert False, "Endpoint wasn't successfully updated"
 
-        # LLT-5532: To be cleaned up...
-        alpha_client.allow_errors(
-            ["telio_proxy::proxy.*Unable to send. Operation not permitted"]
-        )
+    # LLT-5532: To be cleaned up...
+    alpha_client.allow_errors(
+        ["telio_proxy::proxy.*Unable to send. Operation not permitted"]
+    )
 
 
 def node_port(peer_info: Optional[TelioNode]) -> int:
