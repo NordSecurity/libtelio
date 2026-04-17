a7c50a81f661cb6f134d4e0c10436401835bad5e
Gytis Stoškevičius <gytisstoskevicius@gmail.com>
2026-04-17 14:03:32 +0300

    test: replace wg adapter cleanup test with looped clean/dirty shutdown variants
    Replace single-iteration test_wg_adapter_cleanup with two looped tests
    that run 5 iterations each to detect stability issues:
    
    - test_wg_adapter_cleanup_loop_clean_shutdown: verifies repeated
      clean start/stop cycles properly remove WireGuard adapters
    - test_wg_adapter_cleanup_loop_dirty_shutdown: verifies repeated
      dirty shutdowns (taskkill) followed by restarts properly detect
      and clean up orphaned adapters
    
    Extract QUERY_CMD as module-level constant to reduce duplication.
    Add pytest.mark.timeout(240) to both tests.

--- nat-lab/tests/test_wg_adapter.py
+++ nat-lab/tests/test_wg_adapter.py
@@ -2,26 +2,30 @@ import pytest
 from contextlib import AsyncExitStack
 from Pyro5.errors import CommunicationError  # type:ignore
 from tests.helpers import SetupParameters, setup_environment
+from tests.timeouts import TEST_WG_ADAPTER_CLEANUP_TIMEOUT
 from tests.utils.bindings import TelioAdapterType
 from tests.utils.connection import ConnectionTag
 from tests.utils.connection_util import new_connection_by_tag
 from tests.utils.logger import log
 from tests.utils.process import ProcessExecError
 
+QUERY_CMD = [
+    "reg",
+    "query",
+    r"HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}",
+    "/s",
+    "/f",
+    "DeviceInstanceID",
+]
+
 
 @pytest.mark.windows
+@pytest.mark.timeout(TEST_WG_ADAPTER_CLEANUP_TIMEOUT)
 @pytest.mark.parametrize("conn_tag", [ConnectionTag.VM_WINDOWS_1])
-async def test_wg_adapter_cleanup(conn_tag: ConnectionTag):
-    QUERY_CMD = [
-        "reg",
-        "query",
-        r"HKLM\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}",
-        "/s",
-        "/f",
-        "DeviceInstanceID",
-    ]
-    # Run libtelio and kill it dirty so it would leave hanging wg-nt adapter
-    try:
+async def test_wg_adapter_cleanup_loop_clean_shutdown(conn_tag: ConnectionTag):
+    for i in range(5):
+        log.info("Clean shutdown iteration %d", i)
+
         async with AsyncExitStack() as exit_stack:
             env = await exit_stack.enter_async_context(
                 setup_environment(
@@ -41,38 +45,78 @@ async def test_wg_adapter_cleanup(conn_tag: ConnectionTag):
                 in (await conn.create_process(QUERY_CMD).execute()).get_stdout()
             )
 
-            await conn.create_process(
-                ["taskkill", "/T", "/F", "/IM", "python.exe"]
-            ).execute()
-    except (CommunicationError, ConnectionRefusedError, ProcessExecError) as e:
-        log.warning("First libtelio failed with %s", e)
-
-    # Check if libtelio left hanging wg-nt adapter, might now always happen, so we just leave test
-    async with new_connection_by_tag(conn_tag) as conn:
-        if (
-            "WireGuard"
-            not in (await conn.create_process(QUERY_CMD).execute()).get_stdout()
-        ):
-            return
-
-    # Try to start libtelio and see if it properly cleans up orphaned wg-nt adapter and starts normaly
-    async with AsyncExitStack() as exit_stack:
-        env = await exit_stack.enter_async_context(
-            setup_environment(
-                exit_stack,
-                [
-                    SetupParameters(
-                        connection_tag=conn_tag,
-                        adapter_type_override=TelioAdapterType.WINDOWS_NATIVE_TUN,
-                    )
-                ],
+        # After clean exit, verify adapter was properly cleaned up
+        async with new_connection_by_tag(conn_tag) as conn:
+            assert (
+                "WireGuard"
+                not in (await conn.create_process(QUERY_CMD).execute()).get_stdout()
             )
-        )
 
-        conn, *_ = [conn.connection for conn in env.connections]
-        client, *_ = env.clients
 
-        assert (
-            "WireGuard" in (await conn.create_process(QUERY_CMD).execute()).get_stdout()
-        )
-        assert "Removed orphaned adapter" in client.get_stderr()
+@pytest.mark.windows
+@pytest.mark.timeout(TEST_WG_ADAPTER_CLEANUP_TIMEOUT)
+@pytest.mark.parametrize("conn_tag", [ConnectionTag.VM_WINDOWS_1])
+async def test_wg_adapter_cleanup_loop_dirty_shutdown(conn_tag: ConnectionTag):
+    for i in range(5):
+        log.info("Dirty shutdown iteration %d", i)
+
+        # Run libtelio and kill it dirty so it would leave hanging wg-nt adapter
+        try:
+            async with AsyncExitStack() as exit_stack:
+                env = await exit_stack.enter_async_context(
+                    setup_environment(
+                        exit_stack,
+                        [
+                            SetupParameters(
+                                connection_tag=conn_tag,
+                                adapter_type_override=TelioAdapterType.WINDOWS_NATIVE_TUN,
+                            )
+                        ],
+                    )
+                )
+
+                conn, *_ = [conn.connection for conn in env.connections]
+                assert (
+                    "WireGuard"
+                    in (await conn.create_process(QUERY_CMD).execute()).get_stdout()
+                )
+
+                await conn.create_process(
+                    ["taskkill", "/T", "/F", "/IM", "python.exe"]
+                ).execute()
+        except (CommunicationError, ConnectionRefusedError, ProcessExecError) as e:
+            log.warning("Dirty shutdown iteration %d failed with %s", i, e)
+
+        # Check if libtelio left hanging wg-nt adapter
+        async with new_connection_by_tag(conn_tag) as conn:
+            if (
+                "WireGuard"
+                not in (await conn.create_process(QUERY_CMD).execute()).get_stdout()
+            ):
+                log.info(
+                    "No orphaned adapter found after dirty shutdown iteration %d", i
+                )
+                continue
+
+        # Try to start libtelio and see if it properly cleans up orphaned wg-nt adapter
+        async with AsyncExitStack() as exit_stack:
+            env = await exit_stack.enter_async_context(
+                setup_environment(
+                    exit_stack,
+                    [
+                        SetupParameters(
+                            connection_tag=conn_tag,
+                            adapter_type_override=TelioAdapterType.WINDOWS_NATIVE_TUN,
+                        )
+                    ],
+                )
+            )
+
+            conn, *_ = [conn.connection for conn in env.connections]
+            client, *_ = env.clients
+
+            assert (
+                "WireGuard"
+                in (await conn.create_process(QUERY_CMD).execute()).get_stdout()
+            )
+            assert "Removed orphaned adapter" in client.get_stderr()
