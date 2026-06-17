e0eefd4b35e12f1085e726a4bf39ab622b915617
Tomasz Patejko <tomasz.patejko@nordsec.com>
2026-06-17 15:43:20 +0200

    wqnordvpnlite: add connect/disconnect/daemon functionalities

--- clis/nordvpnlite/src/command_listener.rs
+++ clis/nordvpnlite/src/command_listener.rs
@@ -1,14 +1,15 @@
 use std::net::IpAddr;
 
 use clap::Parser;
+// use reqwest::blocking::Client;
 use serde::{Deserialize, Serialize};
 use telio_core::telio_task::io::chan;
 use tokio::sync::oneshot;
 use tracing::{error, info, trace};
-
 use crate::{
     comms::{DaemonConnection, DaemonSocket},
-    config::{Endpoint, RunningConfig},
+    config::{Endpoint, RunningConfig,
+    core_api::get_server_endpoints_list,
     daemon::{NordVpnLiteError, TelioStatusReport},
 };
 
@@ -18,14 +19,18 @@ pub(crate) const TIMEOUT_SEC: u64 = 60;
 #[clap()]
 #[derive(Serialize, Deserialize)]
 pub enum ClientCmd {
+    #[clap(about = "Connects to the endpoint")]
+    Connect,
+    #[clap(about = "Disconnect from the endpoint")]
+    Disconnect,
     #[clap(name = "status", about = "Retrieve the status report")]
     GetStatus,
     #[clap(hide = true)]
     IsAlive,
-    #[clap(name = "stop", about = "Stop daemon execution")]
-    QuitDaemon,
     #[clap(name = "reload", about = "Reload config file and restart the daemon")]
     Reload,
+    #[clap(about = "Show countries with available VPN servers")]
+    Countries,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -41,6 +46,7 @@ pub enum TelioTaskCmd {
     GetStatus(oneshot::Sender<TelioStatusReport>),
     // Connect to exit node with endpoint and optional hostname
     ConnectToExitNode(ExitNodeConfig),
+    DisconnectFromExitNode,
     // Break the receive loop to quit the daemon and exit gracefully
     Quit(oneshot::Sender<()>),
 }
@@ -70,6 +76,10 @@ pub(crate) struct DaemonOpts {
     /// Ignored with no-detach flag.
     #[clap(long = "stdout-path", default_value = "/var/log/nordvpnlite.log")]
     pub stdout_path: String,
+
+    // Do not connect when daemon is running
+    #[clap(long = "do-not-connect")]
+    pub do_not_connect: bool,
 }
 
 /// NordVPN Lite is a lightweight, standalone VPN client built around
@@ -80,11 +90,9 @@ pub(crate) struct DaemonOpts {
 #[command(version = concat!(env!("CARGO_PKG_VERSION"), " (", env!("LIBTELIO_COMMIT_SHA"), ") ", env!("BUILD_PROFILE")))]
 pub enum Cmd {
     #[clap(about = "Runs the nordvpnlite event loop")]
-    Start(DaemonOpts),
-    #[clap(flatten)]
+    Daemon(DaemonOpts),
+    #[command(flatten)]
     Client(ClientCmd),
-    #[clap(about = "Show countries with available VPN servers")]
-    Countries,
 }
 
 /// Command response type used to communicate between `telio runner -> daemon -> client`
@@ -124,13 +132,20 @@ where
     }
 }
 
-pub struct CommandListener {
+pub struct CommandListener<'a> {
     socket: DaemonSocket,
     /// Channel to send commands to telio task
     telio_task_tx: chan::Tx<TelioTaskCmd>,
     config: RunningConfig,
     /// Holds the new running config from a client triggered reload
     pending_config: Option<RunningConfig>,
+}
+
+impl CommandListener {
+    pub fn new(
+        socket: DaemonSocket,
+        telio_task_tx: chan::Tx<TelioTaskCmd>,
+        config: RunningConfig,
 }
 
 impl CommandListener {
@@ -174,21 +189,51 @@ impl CommandListener {
                 })
                 .await
             }
-            ClientCmd::QuitDaemon => {
-                trace!("Quitting telio task");
-                let (response_tx, response_rx) = oneshot::channel();
+            ClientCmd::Connect => {
+                trace!("Connecting to exit node");
+                let (response_tx, _) = oneshot::channel();
+                let command = match get_server_endpoints_list(&self.config).await {
+                    Ok(endpoints) => {
+                        // TODO: LLT-6460 - We can store the recommended server list, just in case
+                        // one server fails to connect, try the next one.
+                        if let Some(endpoint) = endpoints.first() {
+                            debug!("Selected exit node: {:#?}", endpoint);
+                            // initiate the VPN connection
+                            TelioTaskCmd::ConnectToExitNode(ExitNodeConfig {
+                                endpoint: endpoint.to_owned(),
+                                dns: self.config.dns.clone(),
+                            })
+                        } else {
+                            error!("Getting exit node endpoint failed: empty list");
+                            TelioTaskCmd::Quit(response_tx)
+                        }
+                    }
+                    Err(e) => {
+                        error!("Getting exit node endpoint failed: {e}");
+                        TelioTaskCmd::Quit(response_tx)
+                    }
+                };
+
+                // Send the command to the telio task
+                #[allow(mpsc_blocking_send)]
+                self.telio_task_tx.send(command).await.map_err(|e| {
+                    error!("Failed to send exit node command to telio task: {e}");
+                    NordVpnLiteError::CommandFailed(ClientCmd::Connect)
+                })?;
+
+                Ok(CommandResponse::Ok)
+            }
+            ClientCmd::Disconnect => {
                 #[allow(mpsc_blocking_send)]
                 self.telio_task_tx
-                    .send(TelioTaskCmd::Quit(response_tx))
+                    .send(TelioTaskCmd::DisconnectFromExitNode)
                     .await
-                    .map_err(|e| {
-                        error!("Error sending command: {}", e);
-                        NordVpnLiteError::CommandFailed(ClientCmd::QuitDaemon)
+                    .map_err(|_| {
+                        error!("Failed to disconnect from exit node");
+                        NordVpnLiteError::CommandFailed(ClientCmd::Disconnect)
                     })?;
-                // Wait for a response from TelioTask
-                // this essentually blocks the client quit command until the daemon initiated
-                // cleanup
-                handle_response(response_rx, |_| Ok(CommandResponse::Ok)).await
+
+                Ok(CommandResponse::Ok)
             }
             ClientCmd::Reload => {
                 match RunningConfig::from_file(&self.config.path) {
@@ -217,6 +262,25 @@ impl CommandListener {
                     }
                 }
             }
+            ClientCmd::Countries => {
+                unimplemented!()
+            }
+            // ClientCmd::QuitDaemon => {
+            //     trace!("Quitting telio task");
+            //     let (response_tx, response_rx) = oneshot::channel();
+            //     #[allow(mpsc_blocking_send)]
+            //     self.telio_task_tx
+            //         .send(TelioTaskCmd::Quit(response_tx))
+            //         .await
+            //         .map_err(|e| {
+            //             error!("Error sending command: {}", e);
+            //             NordVpnLiteError::CommandFailed(ClientCmd::QuitDaemon)
+            //         })?;
+            //     // Wait for a response from TelioTask
+            //     // this essentually blocks the client quit command until the daemon initiated
+            //     // cleanup
+            //     handle_response(response_rx, |_| Ok(CommandResponse::Ok)).await
+            // }
             ClientCmd::IsAlive => Ok(CommandResponse::Ok),
         }
     }
@@ -251,11 +315,13 @@ impl CommandListener {
                 } else {
                     // Early command handling before TelioTask is initialized
                     match &command {
-                        ClientCmd::QuitDaemon => CommandResponse::Ok,
                         ClientCmd::IsAlive => CommandResponse::Ok,
                         ClientCmd::GetStatus | ClientCmd::Reload => {
                             CommandResponse::DaemonInitializing
                         }
+                        ClientCmd::Connect => CommandResponse::DaemonInitializing,
+                        ClientCmd::Disconnect => CommandResponse::DaemonInitializing,
+                        ClientCmd::Countries => CommandResponse::Ok,
                     }
                 };
                 connection.respond(response.serialize()).await?;
--- clis/nordvpnlite/src/main.rs
+++ clis/nordvpnlite/src/main.rs
@@ -21,6 +21,8 @@ use crate::{
     daemon::NordVpnLiteError,
 };
 
+use tracing::debug;
+
 /// Umask allows only rw-rw-r--
 const DEFAULT_UMASK: u32 = 0o113;
 
@@ -28,78 +30,87 @@ fn main() -> Result<(), NordVpnLiteError> {
     let mut cmd = Cmd::parse();
 
     // Pre-daemonizing setup
-    if let Cmd::Start(opts) = &mut cmd {
-        // Check if daemon already is running before forking
-        if DaemonSocket::get_ipc_socket_path()?.exists() {
-            return Err(NordVpnLiteError::DaemonIsRunning);
-        }
+    match cmd {
+        Cmd::Daemon(opts) => {
+            // Check if daemon already is running before forking
+            if DaemonSocket::get_ipc_socket_path()?.exists() {
+                return Err(NordVpnLiteError::DaemonIsRunning);
+            }
 
-        // Parse config file
-        let mut config = RunningConfig::from_file(&opts.config_path)?;
-        config.parsed.resolve_env_token();
+            // Parse config file
+            let mut config = RunningConfig::from_file(&opts.config_path)?;
+            config.parsed.resolve_env_token();
 
-        println!("Saving logs to: {}", config.parsed.log_file_path);
-        println!("Starting daemon");
+            println!("Saving logs to: {}", config.parsed.log_file_path);
+            println!("Starting daemon");
 
-        // Fork the process before starting Tokio runtime.
-        // Tokio creates a multi-threaded asynchronous runtime,
-        // but during forking only a single thread survives,
-        // leaving tokio runtime in an undefined state and resulting in a panic.
-        // https://github.com/tokio-rs/tokio/issues/4301
-        if !opts.no_detach {
-            // Redirect stdout and stderr to a specified file or /var/log/nordvpnlite.log by default
-            let stdout_log_file = OpenOptions::new()
-                .create(true)
-                .write(true)
-                .truncate(true)
-                .open(&opts.stdout_path)?;
+            // Fork the process before starting Tokio runtime.
+            // Tokio creates a multi-threaded asynchronous runtime,
+            // but during forking only a single thread survives,
+            // leaving tokio runtime in an undefined state and resulting in a panic.
+            // https://github.com/tokio-rs/tokio/issues/4301
+            if !opts.no_detach {
+                // Redirect stdout and stderr to a specified file or /var/log/nordvpnlite.log by default
+                let stdout_log_file = OpenOptions::new()
+                    .create(true)
+                    .write(true)
+                    .truncate(true)
+                    .open(&opts.stdout_path)?;
 
-            // Daemon working directory is set to `/` by default
-            // any relative path operations from now on could fail
-            let daemon = Daemonize::new()
-                .umask(DEFAULT_UMASK)
-                .working_directory(&opts.working_directory)
-                .stdout(stdout_log_file.try_clone()?)
-                .stderr(stdout_log_file);
+                // Daemon working directory is set to `/` by default
+                // any relative path operations from now on could fail
+                let daemon = Daemonize::new()
+                    .umask(DEFAULT_UMASK)
+                    .working_directory(&opts.working_directory)
+                    .stdout(stdout_log_file.try_clone()?)
+                    .stderr(stdout_log_file);
 
-            // Daemonize the process
-            match daemon.execute() {
-                // Quit parent process
-                Outcome::Parent(Ok(_)) => {
-                    return Ok(());
-                }
-                // Continue in child process
-                Outcome::Child(Ok(_)) => {}
-                // Errors
-                Outcome::Parent(Err(err)) => {
-                    eprintln!("Fork parent error: {err}");
-                    return Err(err.into());
-                }
-                Outcome::Child(Err(err)) => {
-                    eprintln!("Child error {err}");
-                    return Err(err.into());
+                // Daemonize the process
+                match daemon.execute() {
+                    // Quit parent process
+                    Outcome::Parent(Ok(_)) => {
+                        return Ok(());
+                    }
+                    // Continue in child process
+                    Outcome::Child(Ok(_)) => {}
+                    // Errors
+                    Outcome::Parent(Err(err)) => {
+                        eprintln!("Fork parent error: {err}");
+                        return Err(err.into());
+                    }
+                    Outcome::Child(Err(err)) => {
+                        eprintln!("Child error {err}");
+                        return Err(err.into());
+                    }
                 }
             }
+
+            let mut logging_handle = logging::setup_logging(
+                &config.parsed.log_file_path,
+                config.parsed.log_level,
+                config.parsed.log_file_count,
+            )?;
+
+            // Run the daemon event loop.
+            let rt = tokio::runtime::Runtime::new()?;
+            rt.block_on(daemon::daemon_event_loop(
+                config,
+                &mut logging_handle,
+                opts.do_not_connect,
+            ))
         }
-
-        let mut logging_handle = logging::setup_logging(
-            &config.parsed.log_file_path,
-            config.parsed.log_level,
-            config.parsed.log_file_count,
-        )?;
-
-        // Run the daemon event loop.
-        let rt = tokio::runtime::Runtime::new()?;
-        rt.block_on(daemon::daemon_event_loop(config, &mut logging_handle))
-    } else {
-        client_main(cmd)
+        Cmd::Client(client_cmds) => client_main(client_cmds),
     }
 }
 
 #[tokio::main]
-async fn client_main(cmd: Cmd) -> Result<(), NordVpnLiteError> {
+async fn client_main(cmd: ClientCmd) -> Result<(), NordVpnLiteError> {
+    use ClientCmd::*;
+
+    debug!("cmd {:?}", cmd);
+
     match cmd {
-        Cmd::Client(cmd) => {
+        Connect | GetStatus | Disconnect => {
             let socket_path = DaemonSocket::get_ipc_socket_path()?;
             if socket_path.exists() {
                 let response = timeout(
@@ -128,17 +139,47 @@ async fn client_main(cmd: Cmd) -> Result<(), NordVpnLiteError> {
                     }
                 }
             } else {
-                match cmd {
-                    ClientCmd::QuitDaemon => {
-                        println!("Daemon is already stopped");
-                        Ok(())
-                    }
-                    _ => Err(NordVpnLiteError::DaemonIsNotRunning),
-                }
+                Err(NordVpnLiteError::DaemonIsNotRunning)
             }
         }
+        // GetStatus => {
+        //     let socket_path = DaemonSocket::get_ipc_socket_path()?;
+        //     if socket_path.exists() {
+        //         let response = timeout(
+        //             Duration::from_secs(TIMEOUT_SEC),
+        //             DaemonSocket::send_command(&socket_path, &serde_json::to_string(&cmd)?),
+        //         )
+        //         .await
+        //         .map_err(|_| NordVpnLiteError::ClientTimeoutError)?;
+
+        //         match CommandResponse::deserialize(&response?)? {
+        //             CommandResponse::Ok => {
+        //                 println!("Command executed successfully");
+        //                 Ok(())
+        //             }
+        //             CommandResponse::StatusReport(status) => {
+        //                 println!("{}", serde_json::to_string_pretty(&status)?);
+        //                 Ok(())
+        //             }
+        //             CommandResponse::DaemonInitializing => {
+        //                 println!("Daemon is not ready, ignoring");
+        //                 Err(NordVpnLiteError::CommandFailed(cmd))
+        //             }
+        //             CommandResponse::Err(e) => {
+        //                 println!("Command executed failed: {e}");
+        //                 Err(NordVpnLiteError::CommandFailed(cmd))
+        //             }
+        //         }
+        //     } else {
+        //         Err(NordVpnLiteError::DaemonIsNotRunning)
+        //     }
+        // }
+        // Disconnect => {
+        //     unimplemented!();
+        //     Err(NordVpnLiteError::InvalidCommand(format!("{cmd:?}")))
+        // }
         // Display list of available countries with VPN servers
-        Cmd::Countries => {
+        Countries => {
             for country in get_countries_with_exp_backoff(None).await? {
                 println!("{}: {}", country.name, country.code);
             }
