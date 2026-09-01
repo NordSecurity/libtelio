85bca64324c18371e582782ca4130f7c87cf616a
Tomasz Kumor <kumor.tomasz@gmail.com>
2026-09-01 12:46:56 +0200

    Add supported_ciphers to exit node Add support for selected_cipher in Event::Node

--- crates/telio-core/src/adapter.rs
+++ crates/telio-core/src/adapter.rs
@@ -121,6 +121,8 @@ impl TryFrom<WgPeer> for telio_wg::uapi::Peer {
 
                 Some(PresharedKey::new(array))
             }),
+            supported_ciphers: None,
+            selected_cipher: None,
         })
     }
 }
