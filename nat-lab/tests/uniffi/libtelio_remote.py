abc78fe5abe52aaaad181db90085f20e08ac38c0
Tomasz Kumor <kumor.tomasz@gmail.com>
2026-09-04 12:37:51 +0200

    nat-lab: test connection to VPN/meshnet with supported ciphers

--- nat-lab/tests/uniffi/libtelio_remote.py
+++ nat-lab/tests/uniffi/libtelio_remote.py
@@ -203,9 +203,9 @@ class LibtelioWrapper:
         self._libtelio.set_tunnel_src_ip(src_ips)
 
     @serialize_error
-    def connect_to_exit_node(self, public_key, allowed_ips: str, endpoint: str):
+    def connect_to_exit_node(self, public_key, allowed_ips: str, endpoint: str, supported_ciphers=None):
         self._libtelio.connect_to_exit_node_with_id(
-            "natlab", public_key, allowed_ips, endpoint, None
+            "natlab", public_key, allowed_ips, endpoint, supported_ciphers
         )
 
     @serialize_error
